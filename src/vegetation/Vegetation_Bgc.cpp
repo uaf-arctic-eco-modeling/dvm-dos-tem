dfa3e8c6facc51c903111bde09576c6a7c326f86
Tobey Carman <tobeycarman@gmail.com>
2013-05-07 11:37:23 -0800

    Manually grabbed patch from Fengming's master.
    Fengming's Note:
    A bug in vegetation_bgc.cpp for maintenance resp.
    This was introduced in the very beginning, but forgot taking it out. It
    affects effectively adjusting 'KRB' for reasonable NPP during
    calibration. So must be corrected.

--- src/vegetation/Vegetation_Bgc.cpp
+++ src/vegetation/Vegetation_Bgc.cpp
@@ -682,9 +682,7 @@ double Vegetation_Bgc::getKr(const double & vegc, const int & ipart){
 	// kr is for calculating maintainance respiration
   	double kr  = 0.;
   	double kra = calpar.kra;
-  	double krb = calpar.krb[ipart]*100.0;
-  	//multipler 100.0 is temporarily set, because 'vegc' here is for leaf, stem, root
-  	// basically it's much small relative to total, then the 'rm' seems too large
+  	double krb = calpar.krb[ipart];
   
   	kr = exp((kra*vegc)+krb);	
   	return kr;
