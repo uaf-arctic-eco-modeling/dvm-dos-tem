f4361a5b6de7ab59c85e57a2f73d9fb29a4d0f77
Ruth Rutter <rarutter@alaska.edu>
2014-03-27 11:03:26 -0800

    larger figure, plots don't share x axis yet

--- calibration/FixedWindow.py
+++ calibration/FixedWindow.py
@@ -39,27 +39,39 @@ class FixedWindow(object):
     self.spcols = spcols
     self.viewport = viewport
 
-    #figure and subplots
-    self.fig, self.axes = plt.subplots(sprows, spcols, sharex='col')
-    self.fig.suptitle(figtitle)
+    self.fig = plt.figure(figsize=(10,8))
+    for row in np.arange(1,self.sprows+1):
+      for col in np.arange(1,self.spcols+1):
+        #rows, columns, which plot
+        self.fig.add_subplot(sprows,spcols,((row-1)*self.spcols+col))
+    self.axes = self.fig.axes
 
+    #figure and subplots
+    #self.fig, self.axes = plt.subplots(sprows, spcols, sharex='col')
+    self.fig.suptitle(figtitle)
+#    self.fig.set_size_inches(8*1.4,6*1.4)
+#    self.fig.set_dpi(90)
+#    self.fig.figure(num=1,figsize=(11,8))
 
     self.traces = traceslist
     self.setup_traces()
-#    for row in np.arange(0,3):
-#      plt.setp([a.set_xlabel('') for a in self.axes[row, :]], visible=False)
+    #plt.setp([a.set_xlabel('') for a in self.axes[1:-self.spcols]], visible=False)
 
     #Prevents label/tick/plot overlapping
-#    self.fig.set_size_inches(9., 7., Forward=True)
-#    self.fig.set_dpi(90)
-    plt.tight_layout()
-    plt.subplots_adjust(bottom=0.09)
+#    plt.tight_layout()
+#    plt.subplots_adjust(bottom=0.09)
+    #left, bottom, right, top, wspace, hspace
+    plt.subplots_adjust(0.06,0.08,0.98,0.94,0.16,0.20)
 
     F = plt.gcf()
     DPI = F.get_dpi()
     DefaultSize = F.get_size_inches()
     print "DPI: ", DPI
     print "Default size: ", DefaultSize
+
+    xinch = DefaultSize[0]
+    yinch = DefaultSize[1]
+
 
     self.ani = animation.FuncAnimation(self.fig, self.update, interval=100,
                                        init_func=self.setup_plot, blit=True)
@@ -102,18 +114,27 @@ class FixedWindow(object):
       #Allow for there to be fewer data points than the width of the viewport
       a.set_data(np.arange(1, artistlength+1),
                  trace['data'][max(0,idx-self.viewport):idx])
+    for ax in self.axes:
+      ax.relim()
+      ax.autoscale()
+      ymin, ymax = ax.yaxis.get_view_interval()
+      #ax.set_ylim(ymin, ymax)
+      #ax.autoscale_view(True,True,True)
     return [trace['artist'][0] for trace in self.traces]
 
   def setup_plot(self):
     '''Initial drawing of plot. Limits & labels set.'''
     fontP = FontProperties()
     fontP.set_size('x-small')
-    for row in self.axes:
-      for ax in row:
-        ax.set_ylim(-30, 100)
-        ax.set_xlim(1, self.viewport)
-        ax.set_xlabel('Months') #ylabel is per trace
-        ax.legend(prop = fontP)
+    for ax in self.axes:
+      #ax.set_ylim(-30, 100)
+      ax.set_xlim(1, self.viewport)
+      ax.legend(prop = fontP, ncol=2,\
+                bbox_to_anchor=(0.5,1.15), loc='upper center')
+      box = ax.get_position()
+    for ax in self.axes[-self.spcols:-1]:
+      ax.set_xlabel('Months')#ylabel is per trace
+    #legend(self.axes[0,1],"testing", loc=(0.85,0.65), prop=fontP)
     return [trace['artist'][0] for trace in self.traces]
 
 
@@ -125,9 +146,9 @@ class FixedWindow(object):
       trace['data'] = empty_container.copy()
       xindex = trace['pnum']/self.spcols
       yindex = trace['pnum']%self.spcols
-      trace['artist'] = self.axes[xindex,yindex].plot([],[], label=trace['jsontag'])
+      trace['artist'] = self.axes[trace['pnum']].plot([],[], label=trace['jsontag'])
       #This will overwrite if the traces have different units. Possible?
-      self.axes[xindex,yindex].set_ylabel(trace['unit'])
+      self.axes[trace['pnum']].set_ylabel(trace['unit'])
 
 
   def show(self):
@@ -141,15 +162,15 @@ if __name__ == '__main__':
     # pnum: which sub plot to be on, 0 based  
     { 'jsontag': 'Rainfall', 'pnum': 0, 'unit': 'mm', },
     { 'jsontag': 'Snowfall', 'pnum': 0, 'unit': 'mm', },
-    { 'jsontag': 'WaterTable', 'pnum': 1, 'unit': 'unit', },
-    { 'jsontag': 'VWCOrganicLayer', 'pnum': 2, 'unit': 'unit', },
+    { 'jsontag': 'WaterTable', 'pnum': 1, 'unit': 'm', },
+    { 'jsontag': 'VWCOrganicLayer', 'pnum': 2, 'unit': 'unit', },#kg/m2?
     { 'jsontag': 'VWCMineralLayer', 'pnum': 2, 'unit': 'unit', },
     { 'jsontag': 'Evapotranspiration', 'pnum' : 3, 'unit': 'unit', },
-    { 'jsontag': 'ActiveLayerDepth', 'pnum': 4, 'unit': 'unit', },
-    { 'jsontag': 'TempAir', 'pnum': 5, 'unit': 'unit', },
-    { 'jsontag': 'TempOrganicLayer', 'pnum': 5, 'unit': 'unit', },
-    { 'jsontag': 'TempMineralLayer', 'pnum': 6, 'unit': 'unit', },
-    { 'jsontag': 'Year', 'pnum': 7, 'unit': 'unit', },
+    { 'jsontag': 'ActiveLayerDepth', 'pnum': 4, 'unit': 'm', },
+    { 'jsontag': 'TempAir', 'pnum': 5, 'unit': 'degrees C', },
+    { 'jsontag': 'TempOrganicLayer', 'pnum': 5, 'unit': 'degrees C', },
+    { 'jsontag': 'TempMineralLayer', 'pnum': 6, 'unit': 'degrees C', },
+    { 'jsontag': 'Year', 'pnum': 7, 'unit': 'W/m2', },#placeholder for PAR
     #{ 'jsontag': '', 'pnum': , 'unit': 'unit', },
   ]
 
