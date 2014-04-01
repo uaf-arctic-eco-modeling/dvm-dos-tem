16a23feb81e65f3c891f29e1b8ae869414afa88f
Tobey Carman <tobeycarman@gmail.com>
2014-04-01 14:29:12 -0800

    better....

--- calibration/ExpandingWindow3.py
+++ calibration/ExpandingWindow3.py
@@ -31,6 +31,7 @@ YRTMPDIR = '/tmp/year-cal-dvmdostem'
 LOG_FORMAT = '%(levelname)-7s %(name)-8s %(message)s'
 logging.basicConfig(level=logging.DEBUG, format=LOG_FORMAT)
 
+
 class ExpandingWindow(object):
   '''An set of expanding window plots that all share the x axis.
   '''
@@ -46,8 +47,7 @@ class ExpandingWindow(object):
     self.fig.suptitle(figtitle)
     
     plt.xlabel("Years")
-  
-  
+    
     x = np.arange(0)
     y = x.copy() * np.nan
   
@@ -55,36 +55,33 @@ class ExpandingWindow(object):
     for trace in self.traces:
       ax = self.axes[ trace['axesnum'] ]
       trace['artists'] = ax.plot(x, y, label=trace['jsontag'])
+    self.relim_autoscale_draw()
+    self.grid_and_legend()
     
-    logging.info("Relimiting and autoscaling...")
-    for ax in self.axes:
-      ax.relim()
-      ax.autoscale()
-      ax.grid()
-      ax.legend()
-    
-    logging.info("Loading em up!")
     self.loademupskis()
-    
+  
     logging.info("Done creating an expanding window plot object...")
 
   def init(self):
     logging.info("Init function for animation")
 
-    #self.loademupskis()
     return [trace['artists'][0] for trace in self.traces]
 
 
-  def report_view_lims(self):
+
+
+  def report_view_and_data_lims(self):
+    logging.info("{0:-^56s}".format("Report Data and View Limits"))
     for i, ax in enumerate(self.axes):
-      (x0,y0),(x1,y1) = ax.viewLim.get_points()
-      logging.info("Axes %i X: %s - %s Y: %s - %s" % (i, x0,x1, y0,y1) )
+      (dx0,dy0),(dx1,dy1) = ax.dataLim.get_points()
+      (vx0,vy0),(vx1,vy1) = ax.viewLim.get_points()
+      logging.info("{0:>10s} {1:>10.3f} {2:>10.3f} {3:>10.3f} {4:>10.3f}".format('Axes%i X:'%i, dx0,dx1, vx0,vx1))
+      logging.info("{0:>10s} {1:>10.3f} {2:>10.3f} {3:>10.3f} {4:>10.3f}".format('Axes%i Y:'%i, dy0,dy1, vy0, vy1))
+
 
 
   def loademupskis(self):
     logging.info("LOAD 'EM UPSKIS!")
-    
-    self.report_view_lims()
     
     files = sorted( glob.glob('%s/*.json' % YRTMPDIR) )
     logging.info("%i json files in %s" % (len(files), YRTMPDIR) )
@@ -136,63 +133,76 @@ class ExpandingWindow(object):
     for trace in self.traces:
       del trace['tmpy']
 
-    self.report_view_lims()
-    self.relim_and_autoscale()
+
+    # ----- RElIMIT and SCALE
+    self.relim_autoscale_draw()
     self.grid_and_legend()
-    self.report_view_lims()
+    self.report_view_and_data_lims()
+  
     logging.info("Done loading 'em upskis.")
+
+
+
 
   def update(self, frame):
     logging.info("Frame %7i" % frame)
+    
+    # get current data and view limits
+    for i, ax in enumerate(self.axes):
+      (dx0,dy0),(dx1,dy1) = ax.dataLim.get_points()
+      (vx0,vy0),(vx1,vy1) = ax.viewLim.get_points()
+    
 
+    files = sorted( glob.glob('%s/*.json' % YRTMPDIR) )
+    logging.info("%i json files in %s" % (len(files), YRTMPDIR) )
+    
+    if int( os.path.basename(files[-1])[0:4] ) > dx1:
+      pass
+      # the largest year file will not fit in the data container...
+      # must resize, then try again
+        
+    else:
+      pass
+      # start at the last file and put it in the
+    
+
+    # create an x range big enough for every possible file...
+    if len(files) == 0:
+      x = np.arange(0)
+    else:
+      end = int( os.path.basename(files[-1])[0:4] )
+      x = np.arange(0, end + 1 , 1) # <-- make range inclusive!
+
+    self.report_view_and_data_lims()
+    
     self.loademupskis()
 
     return [trace['artists'][0] for trace in self.traces]
 
 
-  def relim_and_autoscale(self):
-    logging.info("Relimit axes, autoscale axes.")
+  def relim_autoscale_draw(self):
+    logging.debug("Relimit axes, autoscale axes.")
     for ax in self.axes:
       ax.relim()
       ax.autoscale()
+    logging.info("Redraw plot")
     plt.draw()
 
   def grid_and_legend(self):
-    logging.info("Turn on grid and legend.")
+    logging.debug("Turn on grid and legend.")
     for ax in self.axes:
       ax.grid(True) # <-- w/o parameter, this toggles!!
-      ax.legend()
-
-
-  def pretty_ticks(self):
-    logging.info("Try to setup tick marks so they only fall on easy-to-comprehend month/year intervals...")
-    acceptable_tic_locs_yrs = (1,5,10,20,30,40,50,75,100,200,300,400,500,750,1000,2000,3000,4000)
-    #acceptable_tic_locs_months = [yrs * 12 for yrs in acceptable_tic_locs_yrs ]
-    num_visible_ticks = 7
-    ideal_yrs_per_tick = (self.axes[0].get_xbound()[1] - self.axes[0].get_xbound()[0]) / num_visible_ticks
-    locater_base = min(acceptable_tic_locs_yrs, key=lambda v: abs(v-ideal_yrs_per_tick))
-
-    logging.debug("x axis bounds: %i -> %i" % ( self.axes[0].get_xbound()[0], self.axes[0].get_xbound()[1] ) )
-    logging.debug("ideal # of years per tick mark: %i" % ideal_yrs_per_tick )
-    logging.debug("chosen # of yrs per tick mark: %i" % locater_base )
-
-
-    loc = mplticker.MultipleLocator(base=locater_base)
-
-    for ax in self.axes:
-      ax.xaxis.set_major_locator(loc)
-      ax.grid(True)
+      ax.legend(prop={'size':10})
 
 
   def show(self, dynamic=True):
-    logging.info("Displaying plot with dynamic=%s" % dynamic)
+    logging.info("Displaying plot, dynamic=%s" % dynamic)
 
     if dynamic:
       logging.info("Setup animation.")
-      self.ani = animation.FuncAnimation(self.fig, self.update, interval=1000,
+      self.ani = animation.FuncAnimation(self.fig, self.update, interval=100,
                                        init_func=self.init, blit=True)
   
-    #embed()
     plt.show()
 
       
@@ -217,30 +227,24 @@ if __name__ == '__main__':
   logging.info("Setting up plot properties dictionary...")
   traces = [
     # axesnum: which sub plot to be on, 0 based
-    { 'jsontag': 'GPPAll', 'axesnum': 0, },
-    { 'jsontag': 'NPPAll', 'axesnum': 0, },
+    { 'jsontag': 'GPPAll', 'axesnum': 0, 'pft': 'PFT0', },
+    { 'jsontag': 'NPPAll', 'axesnum': 0, 'pft': 'PFT0', },
 
-    { 'jsontag': 'PARAbsorb', 'axesnum': 1, },
-    { 'jsontag': 'PARDown', 'axesnum': 1, },
+    { 'jsontag': 'GPPAllIgnoringNitrogen', 'axesnum': 1, 'pft': 'PFT0', },
+    { 'jsontag': 'NPPAllIgnoringNitrogen', 'axesnum': 1, 'pft': 'PFT0', },
 
-    { 'jsontag': 'Rainfall', 'axesnum': 2, },
-    { 'jsontag': 'Snowfall', 'axesnum': 2, },
-    
+    { 'jsontag': 'PARAbsorb', 'axesnum': 2, 'pft': 'PFT0', },
+    { 'jsontag': 'PARDown', 'axesnum': 2, 'pft': 'PFT0', },
+
     { 'jsontag': 'WaterTable', 'axesnum': 3, },
 
-    #{ 'jsontag': 'VegCarbon', 'axesnum': 0, },
-    #{ 'jsontag': 'NPPAll', 'axesnum': 0, },
+    { 'jsontag': 'MossdeathCarbon', 'axesnum': 4, },
+    { 'jsontag': 'MossdeathNitrogen', 'axesnum': 4, },
   ]
-  
-  logging.info("Defining which variables are pft specific...")
-  perpftvars = ['GPPAll', 'NPPAll', 'PARAbsorb', 'PARDown']
-  for trace in traces:
-    if trace['jsontag'] in perpftvars:
-      trace['pft'] = 'PFT0'
   
   logging.info("Starting main app...")
  
-  ewp = ExpandingWindow(traces, rows=4, cols=1)
+  ewp = ExpandingWindow(traces, rows=5, cols=1)
 
   ewp.show(dynamic=True)
   
