96140a2db3ed9696e2d946467d2e80882bb6c4d7
Tobey Carman <tobeycarman@gmail.com>
2014-10-30 16:30:59 -0800

    Change RegionInputer to using utility functions.

--- src/input/RegionInputer.cpp
+++ src/input/RegionInputer.cpp
@@ -1,4 +1,10 @@
+
 #include "RegionInputer.h"
+#include "../TEMUtilityFunctions.h"
+#include "../TEMLogger.h"
+
+extern src::severity_logger< severity_level > glg;
+
 
 RegionInputer::RegionInputer() {
 };
@@ -17,47 +23,27 @@ void RegionInputer::init() {
 };
 
 int RegionInputer::initCO2file(string &dir) {
-  //netcdf error
-  NcError err(NcError::silent_nonfatal);
-  co2filename = dir +"co2.nc";
+
+  this->co2filename = dir +"co2.nc";
 
   if (md->runsc) {
-    co2filename = dir +"co2_sc.nc";  //Yuan: read in CO2 ppm from projection
+    BOOST_LOG_SEV(glg, info) << "Running in scenario stage! Make sure that the co2_sc.nc file exists!";
+    this->co2filename = dir +"co2_sc.nc";  //Yuan: read in CO2 ppm from projection
   }
 
-  NcFile co2File(co2filename.c_str(), NcFile::ReadOnly);
+  NcFile co2File = temutil::open_ncfile(this->co2filename);
 
-  if(!co2File.is_valid()) {
-    string msg = co2filename + " is not valid";
-    cout << msg + "\n";
-    //exit(-1);
-    co2filename = dir + "co2.nc";
-    cout << "WARNING: Running with historic co2 (" << co2filename <<")\n";
-  }
+  NcDim* yrD = temutil::get_ncdim(co2File,"YEAR");
 
-  NcDim* yrD = co2File.get_dim("YEAR");
-
-  if(!yrD->is_valid()) {
-    string msg = "YEAR Dimension is not Valid in RegionInputer::initCO2file";
-    cout<<msg+"\n";
-    exit(-1);
-  }
-
-  int yrs = yrD->size();
-  return yrs;
+  return yrD->size();
 }
 
 void RegionInputer::getCO2(RegionData *rd) {
-  NcError err(NcError::silent_nonfatal);
-  NcFile co2File(co2filename.c_str(), NcFile::ReadOnly);
-  NcVar* co2yrV = co2File.get_var("YEAR");
-  NcVar* co2V = co2File.get_var("CO2");
 
-  if(co2yrV==NULL || co2V==NULL) {
-    string msg = "Cannot get CO2 in RegionInputer::getCO2 ";
-    cout<<msg+"\n";
-    exit(-1);
-  }
+  NcFile co2File = temutil::open_ncfile(this->co2filename);
+
+  NcVar* co2yrV = temutil::get_ncvar(co2File, "YEAR");
+  NcVar* co2V = temutil::get_ncvar(co2File, "CO2");
 
   NcBool nb1 = co2yrV->get(&rd->co2year[0], rd->act_co2yr);
   NcBool nb2 = co2V->get(&rd->co2[0], rd->act_co2yr);
@@ -68,7 +54,6 @@ void RegionInputer::getCO2(RegionData *rd) {
     exit(-1);
   }
 
-  co2File.close();
 }
 
 void RegionInputer::setModelData(ModelData* mdp) {
