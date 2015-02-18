7063d2c723cf6cad063bbd3bf86b5d24ddf35be7
Tobey Carman <tobeycarman@gmail.com>
2015-02-18 14:29:25 -0900

    refactor generalized script:
     - add libraries
     - logic for 'aggressive' masking (all px w/ any bad points
       along time axis)
     - attempt some organization to print statements.

--- scripts/detrend-climate-drivers.py
+++ scripts/detrend-climate-drivers.py
@@ -8,28 +8,85 @@
 # ./detrend-climate-data.py jan 10 "../snap-data/tas_mean_C_iem_cccma_cgcm3_1_sresa1b_2001_2100/"
 
 import argparse
+import textwrap   
 import glob
 import os
-import resource
-import subprocess
-import textwrap
+import resource    # for checking available memory
+import subprocess  # for calling various GDAL command line tools 
 
 import rasterio
 import numpy as np
 import scipy.signal
 
 
-def process_data(args):
+def guess_from_filename(f):
 
-  # Make a list with paths to all the right files...
-  monthfiles = glob.glob("%s/*_%02d_*.tif" % (args.infiledir, args.month))
+  f = os.path.basename(f)
+  f_extension = os.path.splitext(f)[1]
+  f_name = os.path.splitext(f)[0]
+
+  # Split into tokens
+  # e.g.: 'tas_mean_C_iem_cccma_cgcm3_1_sresa1b_02_2001.tif'
+  # e.g.: ['tas', 'mean', 'C', 'iem', 'cccma', 'cgcm3', '1', 'sresa1b', '02', '2001']
+  
+  # SNAP's general naming pattern they strive for:
+  # [variable][metric][units][format][assessmentReport][group][model][scenario][timeFrame].[fileFormat]
+  tokens = f_name.split('_')
+
+  variable = tokens[0]
+  metric = tokens[1]
+  # units =   # variable number of '_' separated fields.
+  # group =   # not sure how to choose these yet??
+  # model = 
+  scenario = [-3]
+  year = tokens[-1]
+  month = tokens[-2]
+
+  # Like this: 'tas_mean_C_iem_cccma_cgcm3_1_sresa1b'
+  bname = "_".join(tokens[0:-2]) # everything except the date fields.
+
+  return (bname, variable, metric, scenario, month, year)
+
+def setup_output_directory(orig_file_list):
+  '''Makes a place to put outputs...'''
+
+  if not os.path.exists("detrended_data"):
+    os.makedirs(test)
+
+  #  - mkdir -p detrended_output directory
+  #  - clear any files in detrended_output that match glob (file list)
+  #      * that way other months are preserved but this month can't end up with 
+  #        a mixture of recent and old files if for some reason
+  #        there are some older ones laying around (maybe from a test run 
+  #        or something)
+  # 
+  #  - discover the "basename" for the file - everything but the date fields.
+
+
+  # clear any output
+
+  # vap_mean_hPa_iem_cccma_cgcm3_1_sresa1b_2001_2100.zip
+  # rsds_mean_MJ-m2-d1_iem_cccma_cgcm3_1_sresa1b_2001_2100.zip
+  # pr_total_mm_iem_cccma_cgcm3_1_sresa1b_2001_2100.zip
+  # tas_mean_C_iem_mpi_echam5_sresa1b_2001_2100.zip
+
+
+def main(args):
+
+  '''
+  tas_mean_C_iem_cccma_cgcm3_1_sresa1b_2001_2100/tas_mean_C_iem_cccma_cgcm3_1_sresa1b_01_2001.tif
+  '''
+
+  # Make a list with paths to all the right files.
+  # In this case, one month, all years.
+  monthfiles = sorted(glob.glob("%s/*_%02d_*.tif" % (args.infiledir, args.month)))
   
   TMP_MONTHLISTFILE = 'month-file-list.txt'
   VRTFILE = 'month-%02d.vrt' % args.month
 
   # Write the list to a file...
   with open(TMP_MONTHLISTFILE, 'w') as f:
-    for i in sorted(monthfiles):
+    for i in monthfiles:
       f.write(i)
       f.write("\n")
 
@@ -52,10 +109,14 @@ def process_data(args):
   monthdatafile = rasterio.open(VRTFILE)
 
   # NOTE: should we check for enough memory?? 
-  print resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
+  print "Memory available:", resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
 
-  from IPython import embed; embed()
-  data = monthdatafile.read(range(1,11))
+  # NOTE: one based indexing...sees to be a rasterio thing?
+  # or gdalvrt numbers bands starting at 1? Not sure....
+  # Also, when not debugging, we want to read all the bands
+  # (by not passing any arguments to the read() function. BUT 
+  # this can result in a whole lot of memory usage!
+  data = monthdatafile.read() #<-- empty to read all bands 
 
   # Make sure the mask is set 
   # NOTE: actually not even important for detrending, only
@@ -63,7 +124,9 @@ def process_data(args):
   #       over all the masked points too. But the mask
   #       is key for displaying the data and having the auto-
   #       scaling work correctly...
-  data = np.ma.masked_outside(data, -500, 500, copy=False)
+  data = np.ma.masked_outside(data, -100, 100, copy=False)
+  print "Masked entries [after maskoutside]:           ", np.count_nonzero(data.mask)
+  print 
 
   # Detrend for each pixel over the time axis.
   # For some reason, the detrend function seems to operate on
@@ -72,9 +135,29 @@ def process_data(args):
   # large or very small. These large or small numbers get squared
   # in the detrending fuction and we end up with overflow errors. 
   detrended_data = scipy.signal.detrend(data, axis=0)
+  # print "Masked entries [after detrend]:               ", np.count_nonzero(detrended_data.mask)
+  # print
 
   # Now make sure to add the offset back into the data
   detrended_data[:,:,:] += (data[0,:,:] - detrended_data[0,:,:])
+  # print "Masked entries [after offset]: ", np.count_nonzero(detrended_data.mask)
+  # print
+
+  detrended_data = np.ma.masked_outside(detrended_data, -100, 100)
+  print "Masked entries [after maskoutside]     ",         np.count_nonzero(detrended_data.mask)
+  print 
+
+  # Apply the most aggressive mask to every timeslice.
+  for i, img in enumerate(data[:]):
+      detrended_data.mask[i,:,:] = detrended_data.mask.any(0)
+
+  print "Masked entries [after aggressive mask]  ",        np.count_nonzero(detrended_data.mask)
+  print 
+
+  # NOTE: Really, here I should be writing out a netcdf file as 
+  # that would prevent having to run thru gdal_translate later
+  # to convert the tif to netcdf...
+  # looks like 
 
   # Write out the data to a new series of .tif files
   with rasterio.drivers(CPL_DEBUG=True):
@@ -85,21 +168,47 @@ def process_data(args):
     # Change a few things about the metadata...
     kwargs.update(
       count=1,            # only one band
-      compress='lzw',     # not sure if this is actually helping?
-      driver='GTiff'      # we want .tifs, not .vrts
+      compress='',     # not sure if this is actually helping?
+      driver='netCDF'      # we want .tifs, not .vrts
     )
 
+    outputdir = "detrended_data"
+
+    # Make sure there is a location for outputs
+    if not os.path.exists(outputdir):
+      os.makedirs(outputdir)
+
+    # Clean up any existing output files for this month...
+    existing_file_list = sorted(glob.glob("%s/*_%02d_*.nc" % (outputdir, args.month)))
+    for f in existing_file_list:
+      print "removing %s" % f
+      os.remove(f)
+
+    (bname, variable, metric, scenario, month, year) = guess_from_filename(monthfiles[0])
+
     # Write each timestep out to its own file...
-    for i, timestep in enumerate(detrended_data[:]): 
-      with rasterio.open('month-%02d-%04d.tif' % (args.month, i), 'w', **kwargs) as dst:
+    for i, timestep in enumerate(detrended_data[:]):
+
+      newfname = bname + ("_%02d_%04d.nc" % (args.month, int(year)+i)) 
+      with rasterio.open( os.path.join(outputdir, newfname), 'w', **kwargs) as dst:
         dst.write_band(1, timestep.astype(rasterio.float32))
 
+
+def merge_tifs_to_single_timeseries_netcdf(dir): # path to directory of files...?
+  pass
+  # use the first file in the list to create a empty nc file (time, lon, lat)
+  # dimensions and one record variable
 
 
 if __name__ == '__main__':
 	
   parser = argparse.ArgumentParser(description=textwrap.dedent('''
-    De-trend climate data.
+    De-trend climate data. Takes an input file directory selects 
+    a single month's files for all years. Removes trend along time
+    axis, and writes a new set of netcdf files in the same "shape",
+    (but with slightly different nomenclature: the netcdf file will
+    have dimensions (time, y, x), while the tifs are 2D raster images
+    files, with a single "band" for the variable).
   '''))
 
   parser.add_argument('month', choices=range(1,13), type=int, 
@@ -113,7 +222,7 @@ if __name__ == '__main__':
   args = parser.parse_args()
   print args
 
-  process_data(args)
+  main(args)
 
 
 
