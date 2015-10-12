bf86aa12da281cb09cce3985344f58f37d8d67bf
Ruth Rutter <rarutter@alaska.edu>
2015-10-12 10:59:34 +0000

    Starts organizing tests by when to run

---
+++ tests/master_merge_test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+#Tests to be run on merge to master
+
+
+#Prep
+
+#Build
+
+#Run
+#Is this necessary if immediately preceded by merge to devel?
+
+
+#Package
+#Clean everything prior to zip
+#Zip
+#verify that all prerequisites are installed - what permissions do we have?
+#pack rpm
