4ce610f1c3833053d75a0d33f38d07c762c6fbad
Ruth Rutter <rarutter@alaska.edu>
2015-10-13 10:17:03 +0000

    Adds some comments and mock code for rpm production

--- tests/master_merge_test.sh
+++ tests/master_merge_test.sh
@@ -15,5 +15,13 @@
 #Package
 #Clean everything prior to zip
 #Zip
+#tar --exclude-vcs -czf dvmdostem.tgz .
+#Build RPM tree structure
+#mv dvmdostem.tgz to SOURCES
 #verify that all prerequisites are installed - what permissions do we have?
+#yum-builddep to install dependencies
+#Add openmpi and jsoncpp paths to env variables as necessary
 #pack rpm
+#rpmbuild -ba --define "release x" --define "version x" SPECS/rpmspec
+#Note that the defines above will not be necessary once Jenkins
+# is set up correctly.
