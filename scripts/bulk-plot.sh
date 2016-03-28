2188576d760b7fe871e488fc55573a47e007d8a1
Tobey Carman <tobeycarman@gmail.com>
2016-03-28 12:28:11 -0800

    Update the bulk-plotting script.
     - usage funciton
     - set TAG by cmd line arg
     - don't print multiple copies of non-pft plots
     - loop over array of plot suites

--- scripts/bulk-plot.sh
+++ scripts/bulk-plot.sh
@@ -5,13 +5,79 @@
 # Simple example script for generating a bunch of static plots
 # using the calibration-viewer.py program. Intended to be modified
 # as needed.
+#
+
+# 
+# Command line argument processing
+#
+function usage () {
+	echo "usage: $ ./bulk-plot TAG"
+	echo "Error: " $1
+}
+
+if [[ $# -lt 1 ]]
+then
+	usage "Not enough args"
+	exit
+fi
+
+# Find sha of most recent commit
+SHA=$(git log -1 --oneline | cut -d ' ' -f 1)
+
+TAG="all-mods-$SHA"
+echo "Using TAG: " $TAG
+
+# vv-------- CHANGE SETTINGS AS NEEDED ----vv
 
 NUM_PFTS=7
-SUITE="Soil"
-TAG="oldParams"
-SAVE_LOC="../../pestdemo/tussock_full/results-32e669c-tussock_full"
+TARGET_CMT=5
+SUITES=("Fire" "Soil" "Vegetation" "VegSoil" "Environment" "NCycle")
 
-for i in {0..$NUM_PFTS};
+# Build a tag to use as the directory AND
+# to preprend to each file name (before suite and pft)
+
+# CAUTION - Need to check that this works right no matter
+# where the bulk-plot script is called from!!
+SAVE_LOC="../../output-vault/$TAG"
+
+#
+# vv-------- USER SHOULDN'T NEED TO MODIFY BELOW HERE ---vv
+#
+echo "MAKING THE FOLLOWING DIRECTORY:"
+echo "$SAVE_LOC"
+mkdir -p "$SAVE_LOC"
+
+# Loop over suites and pfts creating and saving a bunch of plots.
+for SUITE in ${SUITES[@]};
 do
-  ../calibration/calibration-viewer.py --suite "$SUITE" --tar-cmtnum 5 --no-show --save-name "$SAVE_LOC/$TAG-$SUITE-pft$i" --pft "$i"
+
+	if [[ "$SUITE" == "Fire" || "$SUITE" == "Environment" ]]
+	then
+	  # echo $SUITE
+	  ../calibration/calibration-viewer.py --suite "$SUITE" \
+	  																		 --tar-cmtnum "$TARGET_CMT" \
+	  																		 --no-show \
+	  																		 --save-name "$SAVE_LOC/$TAG-$SUITE"
+	else
+		for (( I=0; I<=$NUM_PFTS; ++I ))
+		do
+			# echo $SUITE $I
+		  ../calibration/calibration-viewer.py --suite "$SUITE" \
+		  																		 --tar-cmtnum "$TARGET_CMT" \
+		  																		 --no-show \
+		  																		 --save-name "$SAVE_LOC/$TAG-$SUITE-pft$I" \
+		  																		 --pft "$I"
+		done
+	fi
+
 done
+
+function report_args {
+	echo $#
+	echo '0 -->' $0
+	echo '1 -->' $1
+	echo '2 -->' $2
+	echo '3 -->' $3
+	echo '4 -->' $4
+	exit
+}
