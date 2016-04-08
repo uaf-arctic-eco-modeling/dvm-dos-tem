70d5b8041de60b576b721d0b3ccba06273a99149
Tobey Carman <tobeycarman@gmail.com>
2016-04-07 16:27:12 -0800

    Re-work the bulk-plotting script.
     - add command line args for number of pfts, output directory, tag, etc.

--- scripts/bulk-plot.sh
+++ scripts/bulk-plot.sh
@@ -11,73 +11,106 @@
 # Command line argument processing
 #
 function usage () {
-	echo "usage: $ ./bulk-plot TAG"
-	echo "Error: " $1
+  echo "usage: $ ./bulk-plot TAG"
+  echo "       $ ./bulk-plot [--numpfts N] [--sparse] [-h | --help] --outdir PATH --tag TAG"
+  echo "Error: " $1
 }
 
-if [[ $# -lt 1 ]]
+NUM_PFTS=10
+SUITES=("Fire" "Soil" "Vegetation" "VegSoil" "Environment" "NCycle")
+TAG=
+TARGET_CMT=5
+OUTDIR=
+
+while [ "$1" != "" ]; do
+  case $1 in
+    -n | --numpfts )    shift
+                        NUM_PFTS="$1"
+                        ;;
+
+    # useful for debugging so you don't have to 
+    # wait for everything to plot
+    --sparse )          SUITES=("VegSoil")
+                        ;;
+
+    --tag )             shift
+                        TAG="$1"
+                        ;;
+
+    --targetcmt )       shift
+                        TARGET_CMT="$1"
+                        ;;
+
+    --outdir )          shift
+                        OUTDIR="$1"
+                        ;;
+
+    -h | --help )       usage "no error"
+                        exit
+                        ;;
+
+    * )                 usage "Problem with command line arguments!"
+                        exit 1
+  esac
+  shift
+done
+
+if [[ $TAG == "" ]]
 then
-	usage "Not enough args"
-	exit
+  usage "You must supply a tag!"
+  exit 1
+fi
+if [[ $OUTDIR == "" ]]
+  then
+  usage "You must supply a directory for output!"
+  exit 1
 fi
 
-# Find sha of most recent commit
-SHA=$(git log -1 --oneline | cut -d ' ' -f 1)
+if [[ ! -x "calibration/calibration-viewer.py" ]]
+then
+  echo "Cannot find the plotter from here!"
+  echo "Try executing this script ($(basename $0)) from the main dvmdostem directory."
+  exit 1
+fi
 
-TAG="$1-$SHA"
-echo "Using TAG: " $TAG
-
-# vv-------- CHANGE SETTINGS AS NEEDED ----vv
-
-NUM_PFTS=7
-TARGET_CMT=5
-SUITES=("Fire" "Soil" "Vegetation" "VegSoil" "Environment" "NCycle")
-
-# Build a tag to use as the directory AND
-# to preprend to each file name (before suite and pft)
-
-# CAUTION - Need to check that this works right no matter
-# where the bulk-plot script is called from!!
-SAVE_LOC="../../output-vault/$TAG"
+echo "Plotting for pfs 0 to $NUM_PFTS"
+echo "Will plot these suites:"
+for SUITE in ${SUITES[@]}
+do
+  echo "    $SUITE"
+done
+echo "Using TAG: $TAG"
 
 #
-# vv-------- USER SHOULDN'T NEED TO MODIFY BELOW HERE ---vv
+# Finally, start working
 #
-echo "MAKING THE FOLLOWING DIRECTORY:"
-echo "$SAVE_LOC"
+SAVE_LOC="$OUTDIR/$TAG"
+echo "Making directory: $SAVE_LOC"
 mkdir -p "$SAVE_LOC"
 
 # Loop over suites and pfts creating and saving a bunch of plots.
 for SUITE in ${SUITES[@]};
 do
 
-	if [[ "$SUITE" == "Fire" || "$SUITE" == "Environment" ]]
-	then
-	  # echo $SUITE
-	  ../calibration/calibration-viewer.py --suite "$SUITE" \
-	  																		 --tar-cmtnum "$TARGET_CMT" \
-	  																		 --no-show \
-	  																		 --save-name "$SAVE_LOC/$TAG-$SUITE"
-	else
-		for (( I=0; I<=$NUM_PFTS; ++I ))
-		do
-			# echo $SUITE $I
-		  ../calibration/calibration-viewer.py --suite "$SUITE" \
-		  																		 --tar-cmtnum "$TARGET_CMT" \
-		  																		 --no-show \
-		  																		 --save-name "$SAVE_LOC/$TAG-$SUITE-pft$I" \
-		  																		 --pft "$I"
-		done
-	fi
+  if [[ "$SUITE" == "Fire" || "$SUITE" == "Environment" ]]
+  then
+    # echo $SUITE
+    ./calibration/calibration-viewer.py --suite "$SUITE" \
+                                        --tar-cmtnum "$TARGET_CMT" \
+                                        --no-show \
+                                        --save-name "$SAVE_LOC/$TAG-$SUITE"
+  else
+    for (( I=0; I<=$NUM_PFTS; ++I ))
+    do
+      # echo $SUITE $I
+      ./calibration/calibration-viewer.py --suite "$SUITE" \
+                                          --tar-cmtnum "$TARGET_CMT" \
+                                          --no-show \
+                                          --save-name "$SAVE_LOC/$TAG-$SUITE-pft$I" \
+                                          --pft "$I"
+    done
+  fi
 
 done
 
-function report_args {
-	echo $#
-	echo '0 -->' $0
-	echo '1 -->' $1
-	echo '2 -->' $2
-	echo '3 -->' $3
-	echo '4 -->' $4
-	exit
-}
+
