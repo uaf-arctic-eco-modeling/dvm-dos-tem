bae69e9bffe611de8ca4b5d8827d36db52ee057a
Tobey Carman <tobeycarman@gmail.com>
2016-05-02 14:39:34 -0800

    Change moss C initialization.
    Changes soil carbon initialization logic. Previously, we were assigning
    values to the soil layer's C pools (raw, soma, somcr, sompr) based on
    layer thickness (as for other organic layers). With this commit, we
    change to a match our current understanding of the intended structure:
     - There are two moss layers: living (upper) and dead (lower)
     - Moss layers do NOT have the raw, soma, somcr and sompr C pools that
       other soil layers have.
     - The C pool in the moss layer is "dmossc" (dead moss carbon), and is
       set via the parameter variable: initdmossc (cmt_bgcsoil.txt)
     - the thickness of the moss layers comes from parameter

--- src/snowsoil/Soil_Bgc.cpp
+++ src/snowsoil/Soil_Bgc.cpp
@@ -393,11 +393,38 @@ void Soil_Bgc::initOslayerCarbon(double & shlwc, double & deepc) {
       }
 
       if( cumcarbonbot - cumcarbontop > 0.0 ) {
-        if (currl->isOrganic || currl->isMoss) {  // dead moss layers are not regarded as soil organic layers
+        if (currl->isOrganic) {
           currl->rawc  = bgcpar.eqrawc * (cumcarbonbot - cumcarbontop); //note: those eq-fractions of SOM pools must be estimated before
           currl->soma  = bgcpar.eqsoma * (cumcarbonbot - cumcarbontop);
           currl->sompr = bgcpar.eqsompr * (cumcarbonbot - cumcarbontop);
           currl->somcr = bgcpar.eqsomcr * (cumcarbonbot - cumcarbontop);
+        } else if (currl->isMoss) {
+
+          // moss layers are not 'normal' soil organic layers, so contain
+          // no C in the normal soil C pools
+          currl->rawc  = 0.0;
+          currl->soma  = 0.0;
+          currl->sompr = 0.0;
+          currl->somcr = 0.0;
+
+          if (currl->isMoss && currl->nextl->isMoss) {
+
+            // top moss layer, no dead C
+            // (living C is accounted for in the vegetation)
+            ground->moss.dmossc = 0.0;
+
+          } else if (currl->isMoss && !currl->nextl->isMoss) {
+
+            // bottom moss layer, all dead, no living
+            ground->moss.dmossc = chtlu->initdmossc;
+
+          } else {
+            BOOST_LOG_SEV(glg, fatal) << "You should not get here. "
+                                      << "Maybe you have too many moss layers "
+                                      << "specified?";
+            exit(-1);
+          }
+
         } else {
           currl->rawc  = 0.0;
           currl->soma  = 0.0;
