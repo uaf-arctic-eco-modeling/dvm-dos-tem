76da7128fe7422143111d00fbe02a6da81d79282
Ruth Rutter <rarutter@alaska.edu>
2016-11-15 12:51:34 +0000

    Removes RH woody debris from RHSum.
    The previous code didn't take into account that sometimes the first layer is moss, which makes the rhsum calculation a bit off. Ask Helene for specifics.

--- src/snowsoil/Soil_Bgc.cpp
+++ src/snowsoil/Soil_Bgc.cpp
@@ -825,11 +825,15 @@ void Soil_Bgc::deltastate() {
     // layer or first layer if no moss-layer.
     // if ( cd->m_soil.type[il+1] > 0 && (il==0 || cd->m_soil.type[il]==0) ) {
     //   rhsum += del_soi2a.rhmossc;
+    //   Commented out above because rhsum is used to compute the
+    //   dynamic of soil organic matter, which is a different pool
+    //   than the moss carbon pool.
     // }
 
     // All products from debris C decomposition assumed into first layer
+    //Only add woody debris respiration once
     if (il == 0) {
-      rhsum  += del_soi2a.rhwdeb;
+//      rhsum  += del_soi2a.rhwdeb;
     }
 
     // Only calculate these pools for non-moss layers...
