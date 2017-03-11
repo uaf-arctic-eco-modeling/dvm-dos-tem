28604f1823fe2002f79677da9413da0c3d3af40a
Tobey Carman <tobeycarman@gmail.com>
2017-03-10 15:58:05 -0900

    Change logging strategy for monthly and yearly checksumming.
    Change things around so that it is possible to set all the checksumming
    messages to the 'err' log level. Each checksumming function accumulates
    a list of error messages, and then, if there are any errors, it sends each
    item from the list to the log stream.
    
    The code is kind of ugly, but here it seems to work.

--- src/Runner.cpp
+++ src/Runner.cpp
@@ -161,115 +161,133 @@ void Runner::yearly_output(const int year, const std::string& stage,
 
 }
 
-void Runner::log_not_equal(const std::string& a_desc,
-                           const std::string& b_desc,
-                           int PFT,
-                           double A, double B) {
-
+std::string Runner::report_not_equal(const std::string& a_desc,
+                                     const std::string& b_desc,
+                                     int PFT,
+                                     double A, double B) {
+  std::stringstream ss;
   if ( !temutil::AlmostEqualRelative(A, B) ) {
-    BOOST_LOG_SEV(glg, err) << "PFT:" << PFT
-                            << " " << a_desc << " and " << b_desc
-                            << " not summing correctly!"
-                            << " A: "<< A <<" B: "<< B <<" (A-B: "<< A - B <<")";
+    ss << "PFT:" << PFT << " " << a_desc << " and " << b_desc
+       << " not summing correctly!" << " A: "<< A <<" B: "<< B
+       << " (A-B: "<< A - B <<")";
   }
-
+  return ss.str(); // empty string if no error
 }
 
-void Runner::log_not_equal(double A, double B, const std::string& msg) {
-  if ( !temutil::AlmostEqualRelative(A,B) ) {
-    BOOST_LOG_SEV(glg, err) << msg
-                            <<" A: "<< A <<" B: "<< B <<" (A-B: "<< A - B <<")";
+std::string Runner::report_not_equal(double A, double B, const std::string& msg) {
+  std::stringstream ss;
+  if ( !temutil::AlmostEqualRelative(A, B) ) {
+    ss << msg <<" A: "<< A <<" B: "<< B <<" (A-B: "<< A - B <<")";
   }
+  return ss.str(); // empty string if no error
 }
 
 /** Used to check that sums across PFT compartments match the 
     corresponding 'all' container.
 
 */
-void Runner::check_sum_over_compartments() {
+std::list<std::string> Runner::check_sum_over_compartments() {
+
+  std::list<std::string> errlist;
 
   for (int ip = 0; ip < NUM_PFT; ++ip) {
 
-    log_not_equal("whole plant C", "plant C PART", ip,
+    errlist.push_back(report_not_equal(
+                      "whole plant C", "plant C PART", ip,
+                      cohort.bd[ip].m_vegs.call,
+                      cohort.bd[ip].m_vegs.c[I_leaf] +
+                      cohort.bd[ip].m_vegs.c[I_stem] +
+                      cohort.bd[ip].m_vegs.c[I_root]));
+
+    errlist.push_back(report_not_equal("whole plant C", "plant C PART", ip,
                   cohort.bd[ip].m_vegs.call,
                   cohort.bd[ip].m_vegs.c[I_leaf] +
                   cohort.bd[ip].m_vegs.c[I_stem] +
-                  cohort.bd[ip].m_vegs.c[I_root]);
+                  cohort.bd[ip].m_vegs.c[I_root]));
 
-    log_not_equal("whole plant strn", "plant strn PART", ip,
+    errlist.push_back(report_not_equal("whole plant strn", "plant strn PART", ip,
                   cohort.bd[ip].m_vegs.strnall,
                   cohort.bd[ip].m_vegs.strn[I_leaf] +
                   cohort.bd[ip].m_vegs.strn[I_stem] +
-                  cohort.bd[ip].m_vegs.strn[I_root]);
+                  cohort.bd[ip].m_vegs.strn[I_root]));
 
-    log_not_equal("whole plant ingpp", "plant ingpp PART", ip,
+    errlist.push_back(report_not_equal("whole plant ingpp", "plant ingpp PART", ip,
                   cohort.bd[ip].m_a2v.ingppall,
                   cohort.bd[ip].m_a2v.ingpp[I_leaf] +
                   cohort.bd[ip].m_a2v.ingpp[I_stem] +
-                  cohort.bd[ip].m_a2v.ingpp[I_root]);
+                  cohort.bd[ip].m_a2v.ingpp[I_root]));
 
 
-    log_not_equal("whole plant gpp", "plant gpp PART", ip,
+    errlist.push_back(report_not_equal("whole plant gpp", "plant gpp PART", ip,
                   cohort.bd[ip].m_a2v.gppall,
                   cohort.bd[ip].m_a2v.gpp[I_leaf] +
                   cohort.bd[ip].m_a2v.gpp[I_stem] +
-                  cohort.bd[ip].m_a2v.gpp[I_root]);
+                  cohort.bd[ip].m_a2v.gpp[I_root]));
 
-    log_not_equal("whole plant npp", "plant npp PART", ip,
+    errlist.push_back(report_not_equal("whole plant npp", "plant npp PART", ip,
                   cohort.bd[ip].m_a2v.nppall,
                   cohort.bd[ip].m_a2v.npp[I_leaf] +
                   cohort.bd[ip].m_a2v.npp[I_stem] +
-                  cohort.bd[ip].m_a2v.npp[I_root]);
+                  cohort.bd[ip].m_a2v.npp[I_root]));
 
-    log_not_equal("whole plant innpp", "plant innpp PART", ip,
+    errlist.push_back(report_not_equal("whole plant innpp", "plant innpp PART", ip,
                   cohort.bd[ip].m_a2v.innppall,
                   cohort.bd[ip].m_a2v.innpp[I_leaf] +
                   cohort.bd[ip].m_a2v.innpp[I_stem] +
-                  cohort.bd[ip].m_a2v.innpp[I_root]);
+                  cohort.bd[ip].m_a2v.innpp[I_root]));
 
-    log_not_equal("whole plant rm", "plant rm PART", ip,
+    errlist.push_back(report_not_equal("whole plant rm", "plant rm PART", ip,
                   cohort.bd[ip].m_v2a.rmall,
                   cohort.bd[ip].m_v2a.rm[I_leaf] +
                   cohort.bd[ip].m_v2a.rm[I_stem] +
-                  cohort.bd[ip].m_v2a.rm[I_root]);
+                  cohort.bd[ip].m_v2a.rm[I_root]));
 
-    log_not_equal("whole plant rg", "plant rg PART", ip,
+    errlist.push_back(report_not_equal("whole plant rg", "plant rg PART", ip,
                   cohort.bd[ip].m_v2a.rgall,
                   cohort.bd[ip].m_v2a.rg[I_leaf] +
                   cohort.bd[ip].m_v2a.rg[I_stem] +
-                  cohort.bd[ip].m_v2a.rg[I_root]);
+                  cohort.bd[ip].m_v2a.rg[I_root]));
 
-    log_not_equal("whole plant N litterfall", "plant N litterfall PART", ip,
+    errlist.push_back(report_not_equal("whole plant N litterfall", "plant N litterfall PART", ip,
                   cohort.bd[ip].m_v2soi.ltrfalnall + cohort.bd[ip].m_v2soi.mossdeathn,
                   cohort.bd[ip].m_v2soi.ltrfaln[I_leaf] +
                   cohort.bd[ip].m_v2soi.ltrfaln[I_stem] +
-                  cohort.bd[ip].m_v2soi.ltrfaln[I_root]);
+                  cohort.bd[ip].m_v2soi.ltrfaln[I_root]));
 
-    log_not_equal("whole plant C litterfall", "plant C litterfall PART", ip,
+    errlist.push_back(report_not_equal("whole plant C litterfall", "plant C litterfall PART", ip,
                   cohort.bd[ip].m_v2soi.ltrfalcall + cohort.bd[ip].m_v2soi.mossdeathc,
                   cohort.bd[ip].m_v2soi.ltrfalc[I_leaf] +
                   cohort.bd[ip].m_v2soi.ltrfalc[I_stem] +
-                  cohort.bd[ip].m_v2soi.ltrfalc[I_root]);
+                  cohort.bd[ip].m_v2soi.ltrfalc[I_root]));
 
-    log_not_equal("whole plant snuptake", "plant snuptake PART", ip,
+    errlist.push_back(report_not_equal("whole plant snuptake", "plant snuptake PART", ip,
                   cohort.bd[ip].m_soi2v.snuptakeall,
                   cohort.bd[ip].m_soi2v.snuptake[I_leaf] +
                   cohort.bd[ip].m_soi2v.snuptake[I_stem] +
-                  cohort.bd[ip].m_soi2v.snuptake[I_root]);
+                  cohort.bd[ip].m_soi2v.snuptake[I_root]));
 
-    log_not_equal("whole plant nmobil", "plant nmobil PART", ip,
+    errlist.push_back(report_not_equal("whole plant nmobil", "plant nmobil PART", ip,
                   cohort.bd[ip].m_v2v.nmobilall,
                   cohort.bd[ip].m_v2v.nmobil[I_leaf] +
                   cohort.bd[ip].m_v2v.nmobil[I_stem] +
-                  cohort.bd[ip].m_v2v.nmobil[I_root]);
+                  cohort.bd[ip].m_v2v.nmobil[I_root]));
 
-    log_not_equal("whole plant nresorb", "plant nresorb PART", ip,
+    errlist.push_back(report_not_equal("whole plant nresorb", "plant nresorb PART", ip,
                   cohort.bd[ip].m_v2v.nresorball,
                   cohort.bd[ip].m_v2v.nresorb[I_leaf] +
                   cohort.bd[ip].m_v2v.nresorb[I_stem] +
-                  cohort.bd[ip].m_v2v.nresorb[I_root]);
+                  cohort.bd[ip].m_v2v.nresorb[I_root]));
 
   } // end loop over PFTS
+
+  // Remove any empty items...
+  std::list<std::string>::iterator it;
+  for (it = errlist.begin(); it != errlist.end(); it++) {
+    if ( (*it).empty() ) {
+      errlist.erase(it);
+    }
+  }
+  return errlist;
 }
 
 /** Sum across PFTs, compare with ecosystem totals (eg data from 'bdall').
@@ -277,7 +295,7 @@ void Runner::check_sum_over_compartments() {
 Used to add up across all pfts (data held in cohort's bd array of BgcData objects)
 and compare with the data held in cohort's bdall BgcData object.
 */
-void Runner::check_sum_over_PFTs(){
+std::list<std::string> Runner::check_sum_over_PFTs(){
 
   double ecosystem_C = 0;
   double ecosystem_C_by_compartment = 0;
@@ -334,39 +352,63 @@ void Runner::check_sum_over_PFTs(){
 
   }
 
+  std::list<std::string> errlist;
+
   // Check that the sums are equal to the Runner level containers (ecosystem totals)
-  log_not_equal(this->cohort.bdall->m_vegs.call, ecosystem_C, "Runner:: ecosystem veg C not matching sum over PFTs!");
-  log_not_equal(this->cohort.bdall->m_vegs.call, ecosystem_C_by_compartment, "Runner:: ecosystem veg C not matching sum over compartments");
+  errlist.push_back(report_not_equal(this->cohort.bdall->m_vegs.call, ecosystem_C, "Runner:: ecosystem veg C not matching sum over PFTs!"));
+  errlist.push_back(report_not_equal(this->cohort.bdall->m_vegs.call, ecosystem_C_by_compartment, "Runner:: ecosystem veg C not matching sum over compartments"));
 
-  log_not_equal(this->cohort.bdall->m_vegs.nall, ecosystem_N, "Runner:: ecosystem nall not matching sum over PFTs (of strn and nall)!");
-  log_not_equal(this->cohort.bdall->m_vegs.labn, ecosystem_labn, "Runner:: ecosystem labn not matching sum over PFTs!");
-  log_not_equal(this->cohort.bdall->m_vegs.strnall, ecosystem_strn, "Runner:: ecosystem strn not matching sum over PFTs!");
-  log_not_equal(this->cohort.bdall->m_vegs.strnall, ecosystem_strn_by_compartment, "Runner:: ecosystem strn not matching sum over compartments!");
+  errlist.push_back(report_not_equal(this->cohort.bdall->m_vegs.nall, ecosystem_N, "Runner:: ecosystem nall not matching sum over PFTs (of strn and nall)!"));
+  errlist.push_back(report_not_equal(this->cohort.bdall->m_vegs.labn, ecosystem_labn, "Runner:: ecosystem labn not matching sum over PFTs!"));
+  errlist.push_back(report_not_equal(this->cohort.bdall->m_vegs.strnall, ecosystem_strn, "Runner:: ecosystem strn not matching sum over PFTs!"));
+  errlist.push_back(report_not_equal(this->cohort.bdall->m_vegs.strnall, ecosystem_strn_by_compartment, "Runner:: ecosystem strn not matching sum over compartments!"));
 
-  log_not_equal(this->cohort.bdall->m_a2v.ingppall, ecosystem_ingpp, "Runner:: ecosystem npp not matching sum over PFTs!");
-  log_not_equal(this->cohort.bdall->m_a2v.gppall, ecosystem_gpp, "Runner:: ecosystem npp not matching sum over PFTs!");
-  log_not_equal(this->cohort.bdall->m_a2v.innppall, ecosystem_innpp, "Runner:: ecosystem innpp not matching sum over PFTs!");
-  log_not_equal(this->cohort.bdall->m_a2v.nppall, ecosystem_npp, "Runner:: ecosystem npp not matching sum over PFTs!");
+  errlist.push_back(report_not_equal(this->cohort.bdall->m_a2v.ingppall, ecosystem_ingpp, "Runner:: ecosystem npp not matching sum over PFTs!"));
+  errlist.push_back(report_not_equal(this->cohort.bdall->m_a2v.gppall, ecosystem_gpp, "Runner:: ecosystem npp not matching sum over PFTs!"));
+  errlist.push_back(report_not_equal(this->cohort.bdall->m_a2v.innppall, ecosystem_innpp, "Runner:: ecosystem innpp not matching sum over PFTs!"));
+  errlist.push_back(report_not_equal(this->cohort.bdall->m_a2v.nppall, ecosystem_npp, "Runner:: ecosystem npp not matching sum over PFTs!"));
 
-  log_not_equal(this->cohort.bdall->m_v2a.rmall, ecosystem_rm, "Runner:: ecosystem rm not matching sum over PFTs!");
-  log_not_equal(this->cohort.bdall->m_v2a.rgall, ecosystem_rg, "Runner:: ecosystem rg not matching sum over PFTs!");
+  errlist.push_back(report_not_equal(this->cohort.bdall->m_v2a.rmall, ecosystem_rm, "Runner:: ecosystem rm not matching sum over PFTs!"));
+  errlist.push_back(report_not_equal(this->cohort.bdall->m_v2a.rgall, ecosystem_rg, "Runner:: ecosystem rg not matching sum over PFTs!"));
 
-  log_not_equal(this->cohort.bdall->m_v2soi.ltrfalcall, ecosystem_ltrfalc, "Runner:: ecosystem ltrfalc not matching sum over PFTs!");
-  log_not_equal(this->cohort.bdall->m_v2soi.ltrfalnall, ecosystem_ltrfaln, "Runner:: ecosystem ltrfaln not matching sum over PFTs!");
+  errlist.push_back(report_not_equal(this->cohort.bdall->m_v2soi.ltrfalcall, ecosystem_ltrfalc, "Runner:: ecosystem ltrfalc not matching sum over PFTs!"));
+  errlist.push_back(report_not_equal(this->cohort.bdall->m_v2soi.ltrfalnall, ecosystem_ltrfaln, "Runner:: ecosystem ltrfaln not matching sum over PFTs!"));
 
-  log_not_equal(this->cohort.bdall->m_soi2v.snuptakeall, ecosystem_snuptake, "Runner:: ecosystem snuptake not matching sum over PFTs!");
+  errlist.push_back(report_not_equal(this->cohort.bdall->m_soi2v.snuptakeall, ecosystem_snuptake, "Runner:: ecosystem snuptake not matching sum over PFTs!"));
 
-  log_not_equal(this->cohort.bdall->m_v2v.nmobilall, ecosystem_nmobil, "Runner:: ecosystem nmobil not matching sum over PFTs!");
-  log_not_equal(this->cohort.bdall->m_v2v.nresorball, ecosystem_nresorb, "Runner:: ecosystem nresorb not matching sum over PFTs!");
+  errlist.push_back(report_not_equal(this->cohort.bdall->m_v2v.nmobilall, ecosystem_nmobil, "Runner:: ecosystem nmobil not matching sum over PFTs!"));
+  errlist.push_back(report_not_equal(this->cohort.bdall->m_v2v.nresorball, ecosystem_nresorb, "Runner:: ecosystem nresorb not matching sum over PFTs!"));
+
+  // Remove any empty items...
+  std::list<std::string>::iterator it;
+  for (it = errlist.begin(); it != errlist.end(); it++) {
+    if ( (*it).empty() ) {
+      errlist.erase(it);
+    }
+  }
+  return errlist;
 
 }
 
 void Runner::output_caljson_monthly(int year, int month, std::string stage, boost::filesystem::path p){
 
-  BOOST_LOG_SEV(glg, err) << "========== MONTHLY CHECKSUMMING ============";
-  check_sum_over_compartments();
-  check_sum_over_PFTs();
-  BOOST_LOG_SEV(glg, err) << "========== END MONTHLY CHECKSUMMING month: " << month << " year: " << year << "============";
+  std::list<std::string> compartment_err_report = check_sum_over_compartments();
+  std::list<std::string> pft_err_report = check_sum_over_PFTs();
+
+
+  if ( !compartment_err_report.empty() || !pft_err_report.empty() ) {
+    BOOST_LOG_SEV(glg, err) << "========== MONTHLY CHECKSUM ERRORS ============";
+    while (!compartment_err_report.empty()) {
+      BOOST_LOG_SEV(glg, err) << compartment_err_report.front();
+      compartment_err_report.pop_front();
+    }
+    while ( !(pft_err_report.empty()) ){
+      BOOST_LOG_SEV(glg, err) << pft_err_report.front();
+      pft_err_report.pop_front();
+    }
+    BOOST_LOG_SEV(glg, err) << "========== END MONTHLY CHECKSUMMING month: " << month << " year: " << year << " ============";
+  }
+
 
   // CAUTION: this->md and this->cohort.md are different instances!
 
@@ -564,12 +606,21 @@ void Runner::output_caljson_monthly(int year, int month, std::string stage, boos
 
 void Runner::output_caljson_yearly(int year, std::string stage, boost::filesystem::path p) {
 
-  BOOST_LOG_SEV(glg, err) << "========== YEARLY CHECKSUMMING ============";
+  std::list<std::string> compartment_err_report = check_sum_over_compartments();
+  std::list<std::string> pft_err_report = check_sum_over_PFTs();
 
-  check_sum_over_compartments();
-  check_sum_over_PFTs();
-
-  BOOST_LOG_SEV(glg, err) << "========== END YEARLY CHECKSUMMING year: " << year << " ========";
+  if ( !compartment_err_report.empty() || !pft_err_report.empty() ) {
+    BOOST_LOG_SEV(glg, err) << "========== YEARLY CHECKSUM ERRORS ============";
+    while (!compartment_err_report.empty()) {
+      BOOST_LOG_SEV(glg, err) << compartment_err_report.front();
+      compartment_err_report.pop_front();
+    }
+    while ( !(pft_err_report.empty()) ){
+      BOOST_LOG_SEV(glg, err) << pft_err_report.front();
+      pft_err_report.pop_front();
+    }
+    BOOST_LOG_SEV(glg, err) << "========== END YEARLY CHECKSUMMING year: " << year << " ============";
+  }
 
   // CAUTION: this->md and this->cohort.md are different instances!
 
