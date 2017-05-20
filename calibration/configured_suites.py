790271f8e1aeb9f7f77412ee026138cedb2a04d0
Tobey Carman <tobeycarman@gmail.com>
2017-05-19 16:00:46 -0800

    Fix typo on units in configured_suites.py

--- calibration/configured_suites.py
+++ calibration/configured_suites.py
@@ -100,9 +100,9 @@ configured_suites = {
       { 'jsontag': 'VegCarbon', 'axesnum': 2, 'units': 'gC/m^2','pft': '', 'pftpart': 'Root'},
       { 'jsontag': 'LitterfallCarbonAll', 'axesnum': 2, 'units': 'gC/m^2', 'pft': '', },
 
-      { 'jsontag': 'VegStructuralNitrogen', 'axesnum': 3, units='gN/m^2', 'pft': '', 'pftpart': 'Leaf'},
-      { 'jsontag': 'VegStructuralNitrogen', 'axesnum': 3, units='gN/m^2', 'pft': '', 'pftpart': 'Stem'},
-      { 'jsontag': 'VegStructuralNitrogen', 'axesnum': 3, units='gN/m^2', 'pft': '', 'pftpart': 'Root'},
+      { 'jsontag': 'VegStructuralNitrogen', 'axesnum': 3, 'units': 'gN/m^2', 'pft': '', 'pftpart': 'Leaf'},
+      { 'jsontag': 'VegStructuralNitrogen', 'axesnum': 3, 'units': 'gN/m^2', 'pft': '', 'pftpart': 'Stem'},
+      { 'jsontag': 'VegStructuralNitrogen', 'axesnum': 3, 'units': 'gN/m^2', 'pft': '', 'pftpart': 'Root'},
 
       { 'jsontag': 'LitterfallNitrogenPFT', 'axesnum': 4, 'units': 'gN/m^2', 'pft': '', },
       { 'jsontag': 'TotNitrogenUptake', 'axesnum': 4, 'units': 'gN/m^2', 'pft': '', }
