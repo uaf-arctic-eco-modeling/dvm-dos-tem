2bca10a6c906d221171b9bc6bd776a8d06c55af8
rarutter <rarutter@alaska.edu>
2017-12-27 11:37:51 -0900

    Adds a basic script for running with SLURM

---
+++ run_ddt.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -l
+
+# Job name, for clarity
+#SBATCH --job-name="ddtToolikPoC"
+
+# Reservation
+#SBATCH --reservation=snap_8 
+
+# Partition specification
+#SBATCH -p main
+
+# Number of MPI tasks
+#SBATCH -n 100
+
+echo $SBATCH_RESERVATION
+echo $SLURM_JOB_NODELIST
+
+mpirun -n 100 ./dvmdostem -l disabled -p 100 -e 1000 -s 250 -t 109 -n 0
