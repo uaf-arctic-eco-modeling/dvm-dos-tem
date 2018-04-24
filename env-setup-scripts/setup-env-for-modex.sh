83a21831aeaa6d21290f2f854ed449d43cd4ac50
Tobey Carman <tcarman2@alaska.edu>
2018-04-24 17:12:49 -0400

    Update the env setup script and add monkey patch for modex.

--- env-setup-scripts/setup-env-for-modex.sh
+++ env-setup-scripts/setup-env-for-modex.sh
@@ -2,37 +2,33 @@
 
 # setting up to use boost on modex
 echo "Loading module files..."
-module purge; module load python/2.7.13 gcc/4.8.4 boost/1.64.0 netcdf/4.4.1.1-gnu540
+module purge; module load python/2.7.14 gcc/5.4.0 boost/1.67.0 netcdf/4.4.1.1-gnu540
 
 echo "Setting up site specific inlcudes..."
-export SITE_SPECIFIC_INCLUDES="-I/data/software/src/jsoncpp/include/"
+export SITE_SPECIFIC_INCLUDES="-I/data/software/src/jsoncpp_1.8.4/jsoncpp-1.8.4/include/"
 
 echo "Setting up site specific libs..."
-export SITE_SPECIFIC_LIBS="-L/data/software/boost/1.64.0/lib"
+export SITE_SPECIFIC_LIBS="-L/data/software/boost/1.67.0/lib/ -L/data/software/src/jsoncpp_1.8.4/jsoncpp-1.8.4/build-shared/"
 
-# For now don't even need the LD_LIBRARY_PATH variable set -
-# This seems to be smart enough with the proper modules loaded to 
-# pick up the right path to boost etc...
-#export LD_LIBRARY_PATH="/home/UA/rarutter/lib:/home/UA/tcarman2/boost_1_55_0/stage/lib:$LD_LIBRARY_PATH"
+echo "Setting path for loading libraries..."
+export LD_LIBRARY_PATH="/data/software/gcc/gcc-5.4.0/lib64/:/data/software/src/jsoncpp_1.8.4/jsoncpp-1.8.4/build-shared/:/data/software/boost/1.67.0/lib/"
 
-# For some reason we were never able to link to the shared version of 
-# jsoncpp...not sure why?? tried lots of stuff. But in the end, this
-# works, just link directly to the static one.
-echo "Fixing jsoncpp library name in Makefile..."
-sed -e 's:-ljsoncpp\>:/data/software/src/jsoncpp/libs/linux-gcc-4.8.4/libjson_linux-gcc-4.8.4_libmt.a:g' Makefile > Makefile.tmp && mv Makefile.tmp Makefile 
-
-
-echo "NOTE: You may run into issues with a deprecated function in"
-echo "      jsoncpp. Very annoying. Temporary work around is to "
-echo "      comment out the offending line in src/TEMUtilityFunctions.cpp"
-echo "      (in the parse_control_file(..) function)."
+echo "NOTE: With recent versions of jsoncpp, the API changed so that Json::Reader "
+echo "      is no longer a valid use pattern. Fortunatley this was only used once "
+echo "      in our application, in src/TEMUtilityFunctions.cpp, line ~206 in "
+echo "      parse_control_file(...). As of 4/24/2018 we are reluctant to change the "
+echo "      dvmdostem application because it would break all our other intstalls of "
+echo "      jsoncpp and force and upgrade. So what we'll do here is apply a "
+echo "      'monkey patch' on modex so that we can use the latest version."
 echo ""
+git apply env-setup-scripts/modex.monkeypatch.patch
+
 echo "NOTE: This file will NOT work if it is run as a script!"
 echo "      Instead use the 'source' command like this:"
 echo "      $ source env-setup-scripts/setup-env-for-atlas.sh"
 echo ""
-echo "NOTE: Please remember not to commit the modified Makefile!!"
+echo "NOTE: Please remember not to commit the modified src/TEMUtilityFunctions.cpp!!!"
 echo "      You can revert the change with this command:"
-echo "      $ git checkout -- Makefile"
+echo "      $ git checkout -- src/TEMUtilityFunctions.cpp"
 
 
