418294c4a09f7841c66288b921a654c6cf1981c7
Ruth Rutter <rarutter@alaska.edu>
2018-05-02 21:26:03 +0000

    Adds zeroing for snowthick, to prevent monotonic increase.
    This addresses Issue #369, part 1.

--- src/Snow_Env.cpp
+++ src/Snow_Env.cpp
@@ -186,6 +186,7 @@ double  Snow_Env::meltSnowLayersAfterT(Layer* toplayer) {
 // assign double-linked snow horizon data to 'ed'
 void Snow_Env::updateSnowEd(Layer *toplayer) {
   ed->d_snws.swesum = 0.;
+  ed->d_snws.snowthick = 0.;
   Layer* currl=toplayer;
   int snowind = 0;
 
@@ -263,6 +264,7 @@ void Snow_Env::set_state_from_restartdata(const RestartData & rdata) {
   Layer* currl = ground->toplayer;
   int snind = -1;
   ed->d_snws.swesum = 0;
+  ed->d_snws.snowthick = 0;
 
   while(currl!=NULL) {
     if(currl->isSnow) {
