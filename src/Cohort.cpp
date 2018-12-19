d8ecd781dfc97a71c8bc6ad8610062a533a6d9f6
Ruth Rutter <rarutter@alaska.edu>
2018-12-19 09:19:52 +0000

    Modifies NFACTOR based on Klene and Kade.
    These values may still not be precisely what we want, but produce
    results that look better than what we had before.

--- src/Cohort.cpp
+++ src/Cohort.cpp
@@ -428,7 +428,7 @@ void Cohort::updateMonthly_Env(const int & currmind, const int & dinmcurr) {
   //might be more accurate if done correctly, that code had been commented 
   //out for years, and so was removed. 
   //Value from Klene 2001 (summer values) and Kade 2006.
-  edall->d_soid.nfactor = 0.76;
+  edall->d_soid.nfactor = 0.9;
 
   // (ii)Initialize the yearly/monthly accumulators, which are accumulating at the end of month/day in 'ed'
   for (int ip=0; ip<NUM_PFT; ip++) {
@@ -470,6 +470,13 @@ void Cohort::updateMonthly_Env(const int & currmind, const int & dinmcurr) {
                               << " doy=" << doy << ground.layer_report_string("depth thermal hydro ptr");
 
     daylength = temutil::length_of_day(this->lat, doy);
+
+    if(cd.d_snow.numsnwl > 0){
+      edall->d_soid.nfactor = 0.6;
+    }
+    else{
+      edall->d_soid.nfactor = 1.00;
+    }
 
     /* Daily processes for a Cohort, Environmental module...
        Have to use our Climate object to update our EnvData objects's daily
