85ee86556f37b2beae2b8ae1fa3448d188fbf4ee
Tobey Carman <tobeycarman@gmail.com>
2018-12-28 08:34:00 -0700

    Add virtual machine specifications for fedora20 and fedora25.

---
+++ virtualmachines/fedora25/bootstrap-sel-custom.sh
@@ -0,0 +1,109 @@
+#!/bin/bash
+
+#
+# bootstrap-sel-custom.sh
+#
+# Clones SEL repos, sets up dotfiles, (bashrc, vimrc, etc), any other
+# user level settings.
+#
+
+echo "Running bootstrap-sel-custom.sh script..."
+echo "Should not require sudo priviledges to run!"
+
+#
+# Note: Not sure if it is better to use tilde or "$HOME" ??
+#
+
+#
+# add github's key to knownhosts
+#
+if [[ ! -f ~/.ssh/known_hosts ]]; then
+  mkdir -p ~/.ssh
+  touch ~/.ssh/known_hosts
+fi
+
+echo "Appending github's key to ~/.ssh/known_hosts..."
+ssh-keyscan github.com >> "$HOME"/.ssh/known_hosts
+chmod 600 "$HOME"/.ssh/known_hosts
+
+#
+# Clone our software repos...
+#
+if [ ! -d "$HOME"/dvm-dos-tem ]
+then
+  git clone git@github.com:ua-snap/dvm-dos-tem.git "$HOME"/dvm-dos-tem
+fi
+cd dvm-dos-tem
+git remote rename origin upstream
+git checkout devel
+git pull --ff-only upstream devel:devel
+git checkout master
+git pull --ff-only upstream master:master
+cd ..
+
+if [ ! -d "$HOME"/ddtv ]
+then
+  git clone git@github.com:tobeycarman/ddtv.git "$HOME"/ddtv
+fi
+cd ddtv
+git remote rename origin upstream
+git checkout master
+git pull --ff-only upstream master:master
+cd ..
+
+#
+# setup various preference files (dotfiles)
+#
+
+# BASH preferences...
+echo "Setting up bashrc preferences file...."
+cat <<EOF > "$HOME"/.bashrc
+# .bashrc
+
+# Source global definitions
+if [ -f /etc/bashrc ]; then
+  . /etc/bashrc
+fi
+
+# Uncomment the following line if you don't like systemctl's auto-paging feature:
+# export SYSTEMD_PAGER=
+
+# User specific aliases and functions
+
+# Add git branch to bash prompt...
+source /usr/share/git-core/contrib/completion/git-prompt.sh
+export PS1='[\u@\h \W$(declare -F __git_ps1 &>/dev/null && __git_ps1 " (%s)")]\$ '
+
+# set up some environment variables
+export SITE_SPECIFIC_INCLUDES=-I/usr/include/jsoncpp
+export PATH=$PATH:/usr/lib64/openmpi/bin
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib64/openmpi/lib
+
+EOF
+
+# VIM preferences...
+cat <<EOF >> "$HOME"/.vimrc
+syntax on              " this is needed to see syntax
+set ls=2               " allways show status line
+set hlsearch           " highlight searches
+set incsearch          " do incremental searching
+set ruler              " show the cursor position all the time
+set visualbell t_vb=   " turn off error beep/flash
+set ignorecase         " ignore case while searching
+set number             " put numbers on side
+set expandtab          " insert tabs instead of spaces
+set tabstop=2          " use 2 spaces
+set shiftwidth=2       " how many columns to move with reindent operators (>>, <<)
+
+EOF
+
+# GIT prefs..?
+#  - email, editor, color etc??
+
+# Matplotlib prefs...?
+#   - might be able to source a file from a gist online?
+#   e.g. https://gist.github.com/huyng/816622
+
+
+
+echo "DONE setting up a dvm-dos-tem environment. You should be ready to go!"
---
+++ virtualmachines/fedora25/bootstrap-system.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+#
+# bootstrap-system.sh
+#
+# For isntalling any system level software needed to run a dvm-dos-tem
+# development environment.
+#
+# All commands in this script (or the script itself) should be run with sudo!
+#
+
+echo "Installing software..."
+
+#  NOTE: You've gotta install openmpi *after* NetCDF! This keeps NetCDF from
+#  getting setup with some pesky #defines that cause errors when trying to
+#  compile files that include <mpi.h>.
+#  More info: http://www.unidata.ucar.edu/mailing_lists/archives/netcdfgroup/2009/msg00347.html
+dnf install -y git gcc-c++ jsoncpp-devel readline-devel netcdf-devel boost-devel
+dnf install -y openmpi-devel
+
+# Need this to fix the "H5Pset_dxpl_mpio" error that otherwise comes when
+# running IPython and importing netCDF4
+dnf install -y hdf5-openmpi-devel
+
+# this seems to help x11 forwarding
+dnf install -y xauth
+
+# packages used for plotting
+dnf install -y python-matplotlib python-matplotlib-wx netcdf4-python python-ipython
+
+# For processing/preparing the "new style" inputs
+dnf install -y gdal gdal-devel
+
+# For compiling with Scons
+dnf install -y scons
+
+#
+# Bonus - basic functionality should exist w/o these packages and settings
+#
+
+dnf install -y gitk git-gui
+
+# For graphical debugging
+# Note: If gdb is not installed, then QT Creator will complain about "Unable
+#       to create a debugger engine of the type 'no engine'"
+#       Also it may be necessary to install xterm. For some reason Colin and
+#       Tobey's VMs (from this Vagrantfile) had xterm and gdb and Ruth's did
+#       not...Strange?
+dnf install -y gdb xterm qt-creator
+
+# For viewing IPython notebooks, and viewing Netcdf I/O files.
+dnf install -y ncview nco firefox
+
+# Install Doxygen and Graphviz
+# Note: There is a problem with some older versions of Doxygen (specifically
+#       1.8.6, maybe others). The problem is that some classes and functions
+#       don't have complete "Referenced by" lists and some of the links for
+#       "Definition at line ..." links don't show up. This doesn't seem to be
+#       the case with more recent versions of Doxygen, so we install the
+#       "rawhide" repo and install Doxygen from there...
+dnf install -y fedora-release-rawhide
+dnf install -y --enablerepo rawhide doxygen
+dnf install -y graphviz
+
+# The man page conflicts between vim and vim-minimal. Removing vim-minimal
+# takes sudo with it, crippling later attempts at inline provisioning. So we
+# make sure to reinstall sudo.
+su -c "dnf remove -y vim-minimal && dnf install -y vim && dnf install -y sudo"
