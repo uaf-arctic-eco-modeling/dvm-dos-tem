81b5890611ab7fae8eac04a27db1bb3982969520
Tobey Carman <tcarman2@alaska.edu>
2019-09-25 20:06:43 +0000

    refactor, try QCal class.

--- scripts/qcal.py
+++ scripts/qcal.py
@@ -2,35 +2,45 @@
 
 # T Carman May 2019
 
-
 import os
 import glob
 import sys
+import json
 import numpy as np
 
 sys.path.insert(0,"/home/vagrant/dvm-dos-tem/")
-import scripts.output_utils as ou
-from  calibration.calibration_targets import calibration_targets as ct
 
-ctv2 = {'CMT{:02d}'.format(v['cmtnumber']):v for k, v in ct.iteritems()}
+import scripts.output_utils as ou
+import scripts.param_util as pu
+
+from calibration.calibration_targets import calibration_targets as ct
+from calibration.InputHelper import InputHelper
+
+# Paste this into IPython to see log output
+# import logging
+# logger = logging.getLogger()
+# logger.setLevel(logging.DEBUG)
+# logging.debug("test")
+
+# Re-shape the wat the calibration targets dict is structured (keyed)
+caltargets = {'CMT{:02d}'.format(v['cmtnumber']):v for k, v in ct.iteritems()}
 
 
 def qcal_rank(truth, value):
   ''' Deviation from truth, expressed as percent, with sign.''' 
+  #assert(truth > 0)
   return(value/(truth*1.0)-1.0 )
 
 def qcal_rank2(truth, value):
   '''Deviation from truth, expressed as distance squared (no sign).'''
   return (truth - value)**2
 
-#from IPython import embed; embed()
-
 
 data, i = ou.get_last_n_eq("DEEPC", 'yearly', '/home/vagrant/runmanager_rungroups/tem_00000000000/out/00000000000/')
-qcal_rank(ctv2['CMT04']['CarbonDeep'], data[:,0,0].mean())
+qcal_rank(caltargets['CMT04']['CarbonDeep'], data[:,0,0].mean())
 
 data, i = ou.get_last_n_eq("SHLWC", 'yearly', '/home/vagrant/runmanager_rungroups/tem_00000000000/out/00000000000/')
-qcal_rank(ctv2['CMT04']['CarbonShallow'], data[:,0,0].mean())
+qcal_rank(caltargets['CMT04']['CarbonShallow'], data[:,0,0].mean())
 
 
 mm = [
@@ -48,118 +58,65 @@ mm = [
 ('AvailableNitrogenSum','AVLN'),
 ]
 
-'''
-hs = '{:>32s}'.format("calib. variable")
-for i in ctv2['CMT04']['PFTNames']:
-  hs += ('{:>15}'.format(i))
-print hs
 
-# data_path = '/home/vagrant/TESTSAMPLE/output/'
-# data, i = ou.get_last_n_eq("SHLWC", 'yearly', data_path)
+def measure_calibration_quality_nc(output_directory_path):
 
-for ctname, ofname in mm:
-  data, dims = ou.get_last_n_eq(ofname, 'yearly', '/home/vagrant/runmanager_rungroups/tem_00000000000/out/00000000000/')
-  if any('pft' in dim for dim in dims):
-    ds = ''
-    if any('pftpart' in dim for dim in dims):
-      clu = {0:'Leaf', 1:'Stem', 2:'Root'}
-      for c in range(0,3):
-        ds += '{:>32s}'.format('{}({})'.format(ctname, clu[c]))
-        for pft in range(0,10):
-          if ctv2['CMT04']['VegCarbon'][clu[c]][pft] == 0.0: # <-- bad! testing equality of floats! 
-            ds += '{:>15}'.format('--')
-          else:
-            ds += '{:>15.2f}'.format(qcal_rank(ctv2['CMT04'][ctname][clu[c]][pft], data[:,c,pft,0,0].mean()))
-        print ds
-        ds = ''
-    else:
-      ds += '{:>15.2f}'.format(qcal_rank(ctv2['CMT04'][ctname][pft], data[:,pft,0,0].mean()))
-    print ds
-  else:
-    print '{} {}'.format(ctname, qcal_rank(ctv2['CMT04'][ctname], data[:,0,0].mean()))
-'''
+  print "************* WORKING WITH NETCDF FILES ***********"
+  print ""
+  print "WARNING !!! WARNING !!! WARNING !!! HARDCODED CMT NUMBER!!"
+  cmtkey = 'CMT05'
 
+  print "CMT:", cmtkey
 
-'''
-for ctname, ofname in mm:
-  data, dims = ou.get_last_n_eq(ofname, 'yearly', '/home/vagrant/runmanager_rungroups/tem_00000000000/out/00000000000/')
-  if any('pft' in dim for dim in dims):
-    for pft in range(0,10):
-      # if pft == 9 and ctname == 'NPPAllIgnoringNitrogen':
-      #   from IPython import embed; embed()
-      if ctv2['CMT04']['PFTNames'][pft].lower() in 'none,misc,misc.,pft0,pft1,pft2,pft3,pft4,pft5,pft6,pft7,pft8,pft9'.split(","):
-        pass # This PFT is not used...
-      else:
-        if any('pftpart' in dim for dim in dims):
-          clu = {0:'Leaf', 1:'Stem', 2:'Root'}
-          for c in range(0,3):
-            #print ctname, ofname, pft, c, clu[c]
-            if ctv2['CMT04']['VegCarbon'][clu[c]][pft] == 0.0:
-              pass # This pft is not used, or this compartment is not used (i.e. roots for mosses)
-            else:
-              print "[VEG] {} ({}) pft:{} c:{}: {}".format(ctname, ofname, pft, clu[c], qcal_rank(ctv2['CMT04'][ctname][clu[c]][pft], data[:,c,pft,0,0].mean()))
-        else:
-          print "[VEG] {} ({}) pft:{} {}".format(ctname, ofname, pft, qcal_rank(ctv2['CMT04'][ctname][pft], data[:,pft,0,0].mean()))
-  else:
-    #print "Soil", ctname, ofname
-    print "[SOIL] {} ({}): {}".format(ctname, ofname, qcal_rank(ctv2['CMT04'][ctname], data[:,0,0].mean()))
-'''
-# NOTE, TODO, need to figure out how to find CMT type!!
+  qcr = 0.0
+  qcr_2 = 0.0
+  w_qcr = 0.0
+  w_qcr_2 = 0.0
 
-import param_util as pu
+  caltarget_to_ncname_map = [
+    ('GPPAllIgnoringNitrogen','INGPP'),
+    ('NPPAllIgnoringNitrogen','INNPP'),
+    ('NPPAll','NPP'),
+    #('Nuptake','NUPTAKE'), # ??? There are snuptake, lnuptake and innuptake... and TotNitrogentUptake is the sum of sn and ln...
+    ('VegCarbon','VEGC'),
+    ('VegStructuralNitrogen','VEGN'),
+    ('MossDeathC','MOSSDEATHC'),
+    ('CarbonShallow','SHLWC'),
+    ('CarbonDeep','DEEPC'),
+    ('CarbonMineralSum','MINEC'),
+    ('OrganicNitrogenSum','ORGN'),
+    ('AvailableNitrogenSum','AVLN'),
+  ]
 
-qcr = 0.0
-qcr_2 = 0.0
-w_qcr = 0.0
-w_qcr_2 = 0.0
+  print "variable value target rank(abs)"
+  for ctname, ncname in caltarget_to_ncname_map:
 
-for ctname, ofname in mm:
-
-  data, dims = ou.get_last_n_eq(ofname, 'yearly', '/home/vagrant/runmanager_rungroups/tem_00000000000/out/00000000000/')
-  dsizes, dnames = zip(*dims)
-
-  print ctname, ofname, dims, dnames, dsizes
-
-  if dnames == ('time','y','x'):
-    pec = pu.percent_ecosys_contribution('CMT04', ctname)
-    truth = ctv2['CMT04'][ctname]
-    value = data[:,0,0].mean()
     
-    # Unweighted Rank
-    qcr += np.abs(qcal_rank(truth, value))
-    qcr_2 += qcal_rank2(truth, value)
+    data, dims = ou.get_last_n_eq(ncname, 'yearly', output_directory_path)
+    dsizes, dnames = zip(*dims)
 
-    # Weighted Rank
-    w_qcr += np.abs(qcal_rank(truth, value)) * pec
-    w_qcr_2 += qcal_rank2(truth, value) * pec
+    #print ctname, output_directory_path, ncname, dims, dnames, dsizes
 
-  elif dnames == ('time','y','x','pft'):
-    for pft in range(0,10):
-      if pu.is_ecosys_contributor('CMT04', pft):
-        pec = pu.percent_ecosys_contribution('CMT04', ctname, pftnum=pft)
-        truth = ctv2['CMT04'][ctname][pft]
-        value = data[:,pft,0,0].mean()
+    if dnames == ('time','y','x'):
+      pec = pu.percent_ecosys_contribution(cmtkey, ctname)
+      truth = caltargets[cmtkey][ctname]
+      value = data[:,0,0].mean()
+      print ctname, value, truth, np.abs(qcal_rank(truth, value))
+      # Unweighted Rank
+      qcr += np.abs(qcal_rank(truth, value))
+      qcr_2 += qcal_rank2(truth, value)
 
-        # Unweighted Rank
-        qcr += np.abs(qcal_rank(truth, value))
-        qcr_2 += qcal_rank2(truth, value)
+      # Weighted Rank
+      w_qcr += np.abs(qcal_rank(truth, value)) * pec
+      w_qcr_2 += qcal_rank2(truth, value) * pec
 
-        # Weighted Rank
-        w_qcr += np.abs(qcal_rank(truth, value)) * pec
-        w_qcr_2 += qcal_rank2(truth, value) * pec
-      else:
-        pass
-        #print "  -> Failed" # contributor test! Ignoring pft:{}, caltarget:{}, output:{}".format(pft, ctname, ofname)
+    elif dnames == ('time','y','x','pft'):
+      for pft in range(0,10):
+        if pu.is_ecosys_contributor(cmtkey, pft):
+          pec = pu.percent_ecosys_contribution(cmtkey, ctname, pftnum=pft)
+          truth = caltargets[cmtkey][ctname][pft]
+          value = data[:,pft,0,0].mean()
 
-  elif dnames == ('time','y','x','pft','pftpart'):
-    for pft in range(0,10):
-      clu = {0:'Leaf', 1:'Stem', 2:'Root'}
-      for cmprt in range(0,3):
-        #print "analyzing... ctname {} (nc output: {}) for pft {} compartment {}".format(ctname, ofname, pft, cmprt),
-        if pu.is_ecosys_contributor('CMT04', pft, clu[cmprt]):
-          pec = pu.percent_ecosys_contribution('CMT04', ctname, pftnum=pft, compartment=clu[cmprt])
-          truth = ctv2['CMT04'][ctname][clu[cmprt]][pft]
-          value = data[:,cmprt,pft,0,0].mean()
           # Unweighted Rank
           qcr += np.abs(qcal_rank(truth, value))
           qcr_2 += qcal_rank2(truth, value)
@@ -167,23 +124,189 @@ for ctname, ofname in mm:
           # Weighted Rank
           w_qcr += np.abs(qcal_rank(truth, value)) * pec
           w_qcr_2 += qcal_rank2(truth, value) * pec
-
         else:
           pass
-          #print "  -> Failed! "#contributor test! Ignoring pft:{}, compartment:{}, caltarget:{}, output:{}".format(pft, cmprt, ctname, ofname)
+          #print "  -> Failed" # contributor test! Ignoring pft:{}, caltarget:{}, output:{}".format(pft, ctname, ncname)
 
-  else:
-    raise RuntimeError("SOMETHING IS WRONG?")
+    elif dnames == ('time','y','x','pft','pftpart'):
+      for pft in range(0,10):
+        clu = {0:'Leaf', 1:'Stem', 2:'Root'}
+        for cmprt in range(0,3):
+          #print "analyzing... ctname {} (nc output: {}) for pft {} compartment {}".format(ctname, ncname, pft, cmprt),
+          if pu.is_ecosys_contributor(cmtkey, pft, clu[cmprt]):
+            pec = pu.percent_ecosys_contribution(cmtkey, ctname, pftnum=pft, compartment=clu[cmprt])
+            truth = caltargets[cmtkey][ctname][clu[cmprt]][pft]
+            value = data[:,cmprt,pft,0,0].mean()
+            # Unweighted Rank
+            qcr += np.abs(qcal_rank(truth, value))
+            qcr_2 += qcal_rank2(truth, value)
 
-print ""
-print ""
-print "qcr: {}  qcr_2: {}".format(qcr, qcr_2)
-print "w_qcr: {}  w_qcr_2: {}".format(w_qcr, w_qcr_2)
-pu.get_ecosystem_total_C('CMT04')
+            # Weighted Rank
+            w_qcr += np.abs(qcal_rank(truth, value)) * pec
+            w_qcr_2 += qcal_rank2(truth, value) * pec
+
+          else:
+            pass
+            #print "  -> Failed! "#contributor test! Ignoring pft:{}, compartment:{}, caltarget:{}, output:{}".format(pft, cmprt, ctname, ofname)
+
+    else:
+      raise RuntimeError("SOMETHING IS WRONG?")
+
+  print ""
+  print ""
+  print "qcr: {}  qcr_2: {}".format(qcr, qcr_2)
+  print "w_qcr: {}  w_qcr_2: {}".format(w_qcr, w_qcr_2)
+  pu.get_ecosystem_total_C('CMT04')
+
+  return "FUH YEAH"
+
+class QCal(object):
+  def __init__(self,jsondata_path="", ncdata_path="", targets_file="calibration/calibration_targets.py"):
+    #self.cmtkey = ?? # it is possible to lookup the CMT number in the json file, but not the netcdf file!
+    self.targets = caltargets # use the CMT to lookup the targets
+    self.jsondata_path = jsondata_path
+    self.ncdata_path = ncdata_path
+
+
+  def json_qcal(self):
+    ih = InputHelper(self.jsondata_path)
+    assert(os.path.splitext(ih.files()[0])[1] == ".json")
+    measure_calibration_quality_json(ih.files()[-10:])
+
+  def nc_qcal(self):
+    assert(os.path.splitext(os.listdir(self.ncdata_path)[0])[1] == ".nc")
+    measure_calibration_quality_nc(self.ncdata_path)
 
 
 
-#from IPython import embed; embed()
+
+
+def measure_calibration_quality_json(file_list):
+
+  print "************* WORKING WITH JSON FILES ***********"
+  # Figure out which community type was run by looking at the first json file
+  # in the list. Assume that ALL json files have the same CMT!
+  with open(file_list[0]) as f1:
+    f1_data = json.load(f1)
+    cmtkey = f1_data['CMT']
+
+  print "CMT: ", cmtkey
+  qcr_t = 0.0 # A variable for accumulating the total.
+
+  # First process all the non-PFT variables
+  for v in 'MossDeathC,CarbonShallow,CarbonDeep,CarbonMineralSum,OrganicNitrogenSum,AvailableNitrogenSum'.split(','):
+    pec = pu.percent_ecosys_contribution(cmtkey, v)
+    d = 0.0
+    for f in file_list:
+      with open(f) as fh:
+        jdata = json.load(fh)
+      d += jdata[v]/float(len(file_list))
+    qcr = np.abs(qcal_rank(caltargets[cmtkey][v], d))
+    qcr_t += qcr
+    print v, d, caltargets[cmtkey][v], qcr
+    #print "{:>25s} {:>5s} {:>8s} {:0.6f} {:0.3f}".format(v, '', '', pec, qcr)
+
+  # Now process all the PFT variables
+  for ipft, pft in enumerate(['PFT{}'.format(i) for i in range(0,10)]):
+    if pu.is_ecosys_contributor(cmtkey, ipft):
+      for v in ['GPPAllIgnoringNitrogen','NPPAllIgnoringNitrogen','NPPAll']:
+        pec = pu.percent_ecosys_contribution(cmtkey, v, pftnum=ipft)
+        d = 0.0
+        for f in file_list:
+          with open(f) as fh:
+            jdata = json.load(fh)
+          d += jdata[pft][v]/float(len(file_list))
+        qcr = np.abs(qcal_rank(caltargets[cmtkey][v][ipft], d))
+        qcr_t += qcr
+        #print "{:>25s} {:>5d} {:>8s} {:0.6f} {:0.3f}".format(v, ipft, '', pec, qcr)
+    else:
+      pass #print "{:>25s} {:>5d}     --".format(v, ipft)
+
+    # And process all the PFT/Compartment variables
+    for cmprt in ['Leaf','Stem','Root']:
+      if pu.is_ecosys_contributor(cmtkey, ipft, cmprt):
+        for v in ['VegCarbon', 'VegStructuralNitrogen']:
+          pec = pu.percent_ecosys_contribution(cmtkey, v, pftnum=ipft, compartment=cmprt)
+          d = 0.0
+          for f in file_list:
+            with open(f) as fh:
+              jdata = json.load(fh)
+            d += jdata[pft][v][cmprt]/float(len(file_list))
+          qcr = np.abs(qcal_rank(caltargets[cmtkey][v][cmprt][ipft], d))
+          qcr_t += qcr
+        #print "{:>25s} {:>5d} {:>8s} {:0.6f} {:0.3f}".format(v, ipft, cmprt, pec, qcr)
+
+      else:
+        pass #print "{:>25s} {:>5d} {:>8s} {} {}     --".format(v, ipft, cmprt, '','')
+        #print "dict(v='{}',ipft={},cmprt='{}',qcr='{}',pec='{}'')".format(v, ipft,cmprt,"--","--")
+
+  print "Total QCR: {}".format(qcr_t)
+
+
+  return "F YEAH"
+
+
+def print_report(jdata, caltargets):
+
+  cmtkey = jdata['CMT']
+
+  for v in 'MossDeathC,CarbonShallow,CarbonDeep,CarbonMineralSum,OrganicNitrogenSum,AvailableNitrogenSum'.split(','):
+    pec = pu.percent_ecosys_contribution(cmtkey, v)
+    qcr = qcal_rank(caltargets[cmtkey][v], jdata[v])
+    print "{} {}".format(pec, qcr)
+
+  for ipft, pft in enumerate(['PFT{}'.format(i) for i in range(0,10)]):
+    if pu.is_ecosys_contributor(cmtkey, ipft):
+      for v in ['GPPAllIgnoringNitrogen','NPPAllIgnoringNitrogen','NPPAll']:
+        pec = pu.percent_ecosys_contribution(cmtkey, v, pftnum=ipft)
+        qcr = qcal_rank(caltargets[cmtkey][v][ipft], jdata[pft][v])
+
+        print "{} {}".format(pec, qcr)
+
+    for cmprt in ['Leaf','Stem','Root']:
+
+      if pu.is_ecosys_contributor(cmtkey, ipft, cmprt):
+        for v in ['VegCarbon', 'VegStructuralNitrogen']:
+
+          pec = pu.percent_ecosys_contribution(cmtkey, v, pftnum=ipft, compartment=cmprt)
+          qcr = qcal_rank(caltargets[cmtkey][v][cmprt][ipft], jdata[pft][v][cmprt])
+    
+        print "{} {}".format(pec, qcr)
+
+      else:
+        print "--"
+
+
+# NOTE, TODO, need to figure out how to find CMT type!!
+
+
+#datapath = "output/eq-data.tar.gz"
+datapath = "output"
+
+if os.path.isdir(datapath):
+  # Assume this is a directory filled with NetCDF output files
+
+  #assert(os.path.splitext(file_list[0])[1] == ".nc")
+
+  cal_report = measure_calibration_quality_nc(datapath)
+
+elif "data.tar.gz" in datapath:
+
+  ih = InputHelper(datapath)
+
+  assert(os.path.splitext(ih.files()[0])[1] == ".json")
+  cal_report = measure_calibration_quality_json(ih.files()[-10:])
+
+else:
+  RuntimeError("Not sure what to do with this data: {}".format(datapath))
+
+
+print "========================================================================"
+print 
+
+qcal = QCal(jsondata_path="output/eq-data.tar.gz", ncdata_path="output")
+qcal.json_qcal()
+qcal.nc_qcal()
 
 # if __name__ == '__main__':
   
