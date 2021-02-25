3c72a5ca3a1701dab2f748a539459a3b4d9cce6b
Tobey Carman <tobeycarman@gmail.com>
2021-02-25 10:06:41 -0900

    Stub out argument parsing.

--- scripts/ensemble_visualizer.py
+++ scripts/ensemble_visualizer.py
@@ -13,6 +13,7 @@ import matplotlib.pyplot as plt
 import xarray as xr
 #import glob
 #import netCDF4 as nc
+import argparse
 #import scipy.stats as ss
 #from pandas import ExcelWriter
 
@@ -34,6 +35,28 @@ fig.savefig('foo.png')
 
 
 
+
+if __name__ == '__main__':
+
+  parser = argparse.ArgumentParser(
+      description='''Plotting for ensembles of dvmdostem runs.''')
+
+  parser.add_argument('--data',
+    help=textwrap.dedent('''\
+      Path to folder of data, assumes folder has subfolders,
+      ens_*, one subfolder for each ensemble member'''))
+
+  parser.add_argument('--var',
+    help=textwrap.dedent('''\
+      Which variable to plot.'''))
+
+  args = parser.parse_args()
+  print(args)
+
+
+# Ideas for command line interface
+
+# $ ./ensemble_visualizer.py --data /path/to/folder/of/ens/runs --var GPP --yx --stage sp
 
 #GPP.folder[1].plot.scatter(time, )
 #GPP.folder[1].
