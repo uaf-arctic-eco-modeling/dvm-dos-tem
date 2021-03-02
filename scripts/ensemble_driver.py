7a2cbc22756d9b17f1ba54b199910a8472111302
Tobey Carman <tobeycarman@gmail.com>
2021-03-02 05:54:53 -0900

    driver script: add diagnostic print statements, filter DS_Store files from runfolders.

--- scripts/ensemble_driver.py
+++ scripts/ensemble_driver.py
@@ -13,6 +13,8 @@ def adjust_mask(ens_folder_list, exe_path):
     print("Changing runmask for {}".format(folder))
     s = "{}/runmask-util.py --reset --yx 0 0 {}/run-mask.nc".format(exe_path, folder)
     result = subprocess.run(s.split(' '), stdout=subprocess.PIPE, stderr=subprocess.PIPE) #, capture_output=True)
+    if len(result.stderr) > 0:
+      print(result)
 
 def adjust_outvars(ens_folder_list, exe_path):
   ''' add code here to use the outspec_utils.py script '''
@@ -23,8 +25,10 @@ def run(ens_folder_list, exe_path):
   for i, folder in enumerate(ens_folder_list):
     print(i, folder)
     os.chdir(folder)
+    print("Current working directory: ", os.getcwd())
     s = "{}/dvmdostem -p 5 -e 10 -s 15 -f config/config.js --force-cmt 4 -l err".format(os.path.dirname(exe_path))
     print("Run command: ", s)
+    print(s.split(' '))
     result = subprocess.run(s.split(' '), stdout=subprocess.PIPE, stderr=subprocess.PIPE) #, capture_output=True)
     with open('stdout.txt', 'w') as f:
       f.write(result.stdout.decode('utf-8'))
@@ -45,7 +49,9 @@ if __name__ == '__main__':
 
   #runfolders = os.listdir('/home/hannah/dvmdostem-workflows')
   #runfolders = os.listdir('/Users/tobeycarman/Documents/SEL/dvmdostem-workflows')
-  runfolders = os.listdir('/home/UA/tcarman2/dvmdostem-workflows')
+  #runfolders = os.listdir('/home/UA/tcarman2/dvmdostem-workflows')
+  runfolders = os.listdir('/data/workflows')
+  runfolders = [i for i in runfolders if '.DS_Store' not in i]
 
   adjust_mask(runfolders, exe_path)
 
