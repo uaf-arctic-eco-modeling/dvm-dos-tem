4a45dd564bb027a95821c3ef806417b1075835b6
Tobey Carman <tobeycarman@gmail.com>
2021-09-22 12:51:36 -0800

    MAJOR Clean up of notebook...
    ...to only use new SensitivityDriver class.

--- scripts/Docker-sensitivity-workflow.py
+++ scripts/Docker-sensitivity-workflow.py
@@ -20,6 +20,10 @@
 # 5. When done. Shut down container <br/>
 # `$ docker compose down` <br/>
 
+get_ipython().run_line_magic('load_ext', 'autoreload')
+get_ipython().run_line_magic('autoreload', '2')
+
+
 import netCDF4 as nc
 import matplotlib.pyplot as plt
 import numpy as np
@@ -40,9 +44,6 @@ param_specs = [
 ]
 
 
-
-
-
 def generate_sample_matrix(pspecs, N, method='uniform'):
     
     if not method == 'uniform':
@@ -52,32 +53,74 @@ def generate_sample_matrix(pspecs, N, method='uniform'):
     for i, p in enumerate(filter(lambda x: x['enabled'], param_specs)):
         samples = np.linspace(p['bounds'][0], p['bounds'][1], N)
         sample_matrix[p['name']] = samples
+
     return pd.DataFrame(sample_matrix)
 
 
-'{:04d}'.format(1)
+
 
 
 sample_matrix = generate_sample_matrix(param_specs, 10)
-# print(sample_matrix)
-# len(sample_matrix)
-# for i, row in enumerate(sample_matrix):
-#     print(i, row)
-    
-# print()
-# for row in sample_matrix.iterrows():
-#     print(row)
+sample_matrix
 
 
-x2 = Sensitivity.SensitivityDriver(param_specs, sample_matrix)
+driver = Sensitivity.SensitivityDriver(param_specs, sample_matrix)
 
 
-x2.params
-x2.setup_multi()
+driver.setup_multi()
+
+
+driver.run_all_samples()
+
+
+
+
+
+driver.collect_outputs()
+
+
+import glob
+file_list = glob.glob('/data/workflows/sensitivity_analysis/**/*sensitivity.csv', recursive=True)
+df = pd.concat( map(pd.read_csv, file_list), ignore_index=True)
+df = df.sort_values('p_cmax')
+corr = df.corr()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 # Sets initial values from the parameters directory - works better when used with 
-# Sensitivity.setup_single()  
+# Sensitivity.setup_single() 
+# Not sure how to use this yet...
 for param in param_specs:
     pfile = pu.which_file(os.path.join(x2.work_dir, "parameters"), param['name'])
     data = pu.get_CMT_datablock(pfile, param['cmtnum'])
@@ -92,10 +135,6 @@ for param in param_specs:
     print(param)
 
 
-#x2.run()
-
-
-get_ipython().system('ls {x2.work_dir}')
 
 
 
@@ -110,108 +149,44 @@ get_ipython().system('ls {x2.work_dir}')
 
 
 
-x = Sensitivity.Sensitivity()
-x.setup()
 
 
 
 
 
-get_ipython().system('ls /data/workflows/sensitivity_analysis/')
-
-
-x.params
-
-
-x.run_model()
-# x.write_defaults()
-# x.params
-
-#sensitivity_defaults.csv
-# pname,pvalue,output_gpp,output_vegc
-# cmax,100,164.25517511367798,443.6672061284383
-# rhq10,0.5,164.25517511367798,443.6672061284383
-# vcmax,200,164.25517511367798,443.6672061284383
-
-# sensitivity_default.csv
-# output_gpp, output_vegc
-#x.write_param_dict()
-
-x.collect_outputs({'name':'default','file':''})
-
-
-#!cat /data/workflows/sensitivity_analysis/default_param_values.csv
-print()
-# pname,default_value
-# cmax,100
-# rhq10,0.5
-
-
-get_ipython().system('cat /data/workflows/sensitivity_analysis/sensitivity_default.csv')
-print()
-get_ipython().system('cat /data/workflows/sensitivity_analysis/sensitivity_cmax.csv')
-print()
-get_ipython().system('cat /data/workflows/sensitivity_analysis/sensitivity_rhq10.csv')
 
 
 
 
 
-# Work in progress....
-for param_dict in x.params:
-    for sample in param_dict['samples']:
-        x.update_param(sample, param_dict['name'])
-        x.run_model()
-        x.collect_outputs(param_dict)
-
-
-get_ipython().system('ls {x.work_dir}')
-
-
-get_ipython().system('cat /data/workflows/sensitivity_analysis/sensitivity_default.csv')
-print()
-get_ipython().system('cat /data/workflows/sensitivity_analysis/sensitivity_cmax.csv')
-print()
-get_ipython().system('cat /data/workflows/sensitivity_analysis/sensitivity_rhq10.csv')
 
 
 
 
 
-df_cmax = pd.read_csv('{:}/sensitivity_cmax.csv'.format(x.work_dir))
-df_rhq10 = pd.read_csv('{}/sensitivity_rhq10.csv'.format(x.work_dir))
-df_default = pd.read_csv('{}/sensitivity_default.csv'.format(x.work_dir))
-print(df_cmax.head())
-print(df_rhq10.head())
-print(df_default.head())
 
 
-fig.clear()
 
 
-fig, axes = plt.subplots(2,2)
-axes[0,0].plot(df_cmax.pvalue, df_cmax.output_gpp, marker='.')
-axes[1,0].plot(df_cmax.pvalue, df_cmax.output_vegc)
-
-axes[0,1].plot(df_rhq10.pvalue, df_rhq10.output_gpp)
-axes[1,1].plot(df_rhq10.pvalue, df_rhq10.output_vegc)
-
-#axes[0].plot(df.pvalue[1:], df.output_gpp[1:])
-#axes[0].plot(df.pvalue[0], df.output_gpp[0], marker='^', color='red')
-#axes[0].plot(df.pvalue[1:], df.output_vegc[1:])
-
-#axes[1].plot(df.pvalue[1:], df.output_vegc[1:])
-#axes[1].plot(df.pvalue[0], df.output_vegc[0], marker='^', color='red')
-
-#df.plot('pvalue','output_gpp', 'output_vegc')
-#df.plot('pvalue','output_vegc')
 
 
-gpp_sens_df = pd.DataFrame(dict(pvalue=df.pvalue[1:], sensitivity=abs(df.output_gpp[0] - df.output_gpp[1:])/abs(df.output_gpp[0])))
-vegc_sens_df = pd.DataFrame(dict(pvalue=df.pvalue[1:], sensitivity=abs(df.output_vegc[0] - df.output_vegc[1:])/abs(df.output_vegc[0])))
 
-axes = gpp_sens_df.plot('pvalue', 'sensitivity', kind='line', ylabel='% error from default')
-axes = vegc_sens_df.plot('pvalue', 'sensitivity', kind='line', ylabel='% error from default')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
