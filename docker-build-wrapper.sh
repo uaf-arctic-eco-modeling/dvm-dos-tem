fb2c8d9311e6765e963f7aa24f68c49bac24c45b
Tobey Carman <tobeycarman@gmail.com>
2022-08-08 13:19:59 -0800

    Refactor: docker-build-wrapper.sh, git describe output to variable

--- docker-build-wrapper.sh
+++ docker-build-wrapper.sh
@@ -6,9 +6,10 @@
 # Note: You may try --no-cache with the docker build commands if they
 # fail with various errors reccomending --fix-missing errors.
 
+GIT_VERSION=$(git describe)
 # Makes a general development image with various dev tools installed:
 # e.g. compiler, make, gdb, etc
-docker build --build-arg GIT_VERSION=$(git describe) --target cpp-dev --tag cpp-dev:$(git describe) .
+docker build --build-arg GIT_VERSION=$GIT_VERSION --target cpp-dev --tag cpp-dev:$GIT_VERSION .
 
 # Makes the specific dvmdostem development image with dvmdostem
 # specific dependencies installed, e.g:
@@ -16,13 +17,13 @@ docker build --build-arg GIT_VERSION=$(git describe) --target cpp-dev --tag cpp-
 # Intention is that your host machine's repo will be mounted 
 # as a volume at /work, and you can use this container as 
 # a compile time and run time environment.
-docker build --build-arg GIT_VERSION=$(git describe) --target dvmdostem-dev --tag dvmdostem-dev:$(git describe) .
+docker build --build-arg GIT_VERSION=$GIT_VERSION --target dvmdostem-dev --tag dvmdostem-dev:$GIT_VERSION .
 
 # An image with the compiled dvmdostem binary program inside it
 # Intention is to use this purely as a compile time environment
 # used to create the dvmdostem binary so that it can be copied into
 # the lean run image.
-docker build --build-arg GIT_VERSION=$(git describe) --target dvmdostem-build --tag dvmdostem-build:$(git describe) .
+docker build --build-arg GIT_VERSION=$GIT_VERSION --target dvmdostem-build --tag dvmdostem-build:$GIT_VERSION .
 
 # A lean images with only the bare minimum stuff to run dvmdostem
 # Does NOT have development tools, compilers, editors, etc
@@ -31,9 +32,9 @@ docker build --build-arg GIT_VERSION=$(git describe) --target dvmdostem-build --
 # local directory on this host. Then when building the run image, we
 # copy the binary from local host into the image.
 docker run --rm --volume $(pwd):/work dvmdostem-dev:$(git describe) make
-docker build --build-arg GIT_VERSION=$(git describe) --target dvmdostem-run --tag dvmdostem-run:$(git describe) .
 
+docker build --build-arg GIT_VERSION=$GIT_VERSION --target dvmdostem-run --tag dvmdostem-run:$GIT_VERSION .
 
 
 # The bastard step child needed to run various gdal tools
-docker build --build-arg GIT_VERSION=$(git describe) --tag dvmdostem-mapping-support:$(git describe) -f Dockerfile-mapping-support .
+docker build --build-arg GIT_VERSION=$GIT_VERSION --tag dvmdostem-mapping-support:$GIT_VERSION -f Dockerfile-mapping-support .
