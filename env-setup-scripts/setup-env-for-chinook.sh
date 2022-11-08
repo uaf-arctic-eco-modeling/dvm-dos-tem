2e88d14f3282fb8c02cd89d6228613fd5e18dce3
Ruth Rutter <rarutter@alaska.edu>
2022-11-07 23:27:35 -0800

    Updates hardcoded paths for mpi on Chinook

--- env-setup-scripts/setup-env-for-chinook.sh
+++ env-setup-scripts/setup-env-for-chinook.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 
-export PATH="$HOME/custom_software/openmpi-3.0.0/bin:$PATH"
+export PATH="$HOME/custom_software/openmpi-4.1.0/bin:$PATH"
 
-export LD_LIBRARY_PATH="$HOME/custom_software/openmpi-3.0.0/lib:$HOME/custom_software/boost_1_55_0/lib:$HOME/custom_software/jsoncpp/libs/linux-gcc-4.4.7:$HOME/custom_software/hdf5-1.8.19/hdf5/lib:$HOME/custom_software/netcdf-4.4.1.1/netcdf/lib:$LD_LIBRARY_PATH"
+export LD_LIBRARY_PATH="$HOME/custom_software/openmpi-4.1.0/lib:$HOME/custom_software/boost_1_55_0/lib:$HOME/custom_software/jsoncpp/libs/linux-gcc-4.4.7:$HOME/custom_software/hdf5-1.8.19/hdf5/lib:$HOME/custom_software/netcdf-4.4.1.1/netcdf/lib:$LD_LIBRARY_PATH"
 
 
 echo "NOTE: This file will NOT work if it is run as a script!"
