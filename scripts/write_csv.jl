5a6ad72254dcf1ae5b54c81bca38946d0319baf9
Aiza Kabeer <akabeer@woodwellclimate.org>
2023-01-12 06:10:17 +0000

    bug fixes and soil parameters check

--- scripts/write_csv.jl
+++ scripts/write_csv.jl
@@ -38,6 +38,7 @@ function save_csv(paramkey,paramdist_min,paramdist_max,initial_guess,problemname
         CSV.write(problemname*"_2.csv", final_results)
     else
         CSV.write(problemname*".csv", final_results)
+        print(problemname*".csv is saved.")
     end
 end
 
@@ -48,15 +49,27 @@ function get_itr_results(problemname)
     lines=multi_cal.read_results(problemname*".finalresults");
     #extract out final param results
     merged_params=multi_cal.create_dict_params(lines);
-    #add cols to original df, save csv
     df2=permutedims(DataFrame(merged_params));
     # set OF error as column headers
     err=multi_cal.read_error(problemname*".finalresults");
-    rename!(df2, err)
+    rename!(df2, err, makeunique=true)
+    cols=size(df2,2)
+    #if there are soil params, due to physical constraints, need to satisfy: Kdcraw > Kdcactive > Kdcpr > Kdccr
+    if haskey(merged_params,"kdcrawc") 
+        for nn in 1:cols
+            if ~(df2[end-3,nn] > df2[end-2,nn] > df2[end-1,nn] > df2[end,nn])
+                # assumes params are in this order at end: kdcrawc, kdcsoma, kdcsompr, kdcsomcr
+                print("Calibration run "*nn*"does not meet soil parameters physical criteria. Removing results...")
+                #discard results that don't meet criteria
+                df2=select!(df2, Not(:nn))
+            end
+        end
+    end
+    return df2
 end
 
 # targets/obs are in md["Observations"].vals
-# model data is forward_predictions.vals(vector) or forward_predictions (array) for random cals
+# model data is forward_predictions.vals (vector) or forward_predictions (array) for random cals
 # labels are Mads.gettargetkeys(md) or forward_predictions.keys
 function save_model_csv(md,problemname,forward_predictions)
     m=(md["Observations"])
@@ -67,7 +80,11 @@ function save_model_csv(md,problemname,forward_predictions)
         t = Mads.gettarget(m[o])
         push!(x, t)
     end
-    df = DataFrame(parameters = params, observation = x, model = forward_predictions[:,1])
+    #for single calibration runs (Non random) we have not collected dict values into an array:
+    if ~isa(forward_predictions,Array) 
+        forward_predictions=collect(values(forward_predictions))
+    end
+    df = DataFrame(parameters = keys, observation = x, model = forward_predictions[:,1])
     cols=size(forward_predictions,2)
     if cols>1 
         for nn in 2:cols
@@ -76,4 +93,5 @@ function save_model_csv(md,problemname,forward_predictions)
         end
     end
     CSV.write(problemname*"_model.csv", df)
+    print(problemname*"_model.csv is saved.")
 end
