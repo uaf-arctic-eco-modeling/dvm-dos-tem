cf7945067211e888730a66f18fb47d8de84e2257
Elchin <elchin.jafarov@gmail.com>
2023-03-02 22:20:20 +0000

    updated to fully generic step1

--- scripts/AC3-STEP1-MD1.jl
+++ scripts/AC3-STEP1-MD1.jl
@@ -5,7 +5,7 @@
 # targets: (GPP) grabs all targets except last 6 elements correspoding to soil parameters 
 
 import Mads
-import Pkg; Pkg.add("YAML")
+#import Pkg; Pkg.add("YAML")
 import YAML
 import PyCall
 @show pwd()
@@ -21,30 +21,74 @@ dvmdostem.set_params(dvmdostem.cmtnum, dvmdostem.paramnames, dvmdostem.pftnums)
 
 """
 
+mads_config = YAML.load_file("config-step1-md1.yaml")
+
 function TEM_pycall(parameters::AbstractVector)
         predictions = PyCall.py"dvmdostem.run_TEM"(parameters)
         return predictions
 end
 
-mads_config = YAML.load_file("config-step1-md1.yaml")
 initial_guess=mads_config["mads_initial_guess"]
 
-y_init=PyCall.py"dvmdostem.run_TEM"(initial_guess)
-#print("length y_init:  ",length(y_init))
-#print(y_init)
+#y_init=PyCall.py"dvmdostem.run_TEM"(initial_guess)
 obs=PyCall.py"dvmdostem.get_targets"()
 n_o=length(obs)
-obs_time=1:n_o-6 #excluding soil carbon values
-targets=obs[1:n_o-6]#grabbing only vegetation targets
-#print("obs_length: ",length(obs))
-#print(obs[1:n_o])
-#print(obs[1:n_o-6])
+obstime=1:n_o-6             #excluding soil carbon values
+targets=obs[1:n_o-6]        #grabbing only vegetation targets
+
+# check for obsweight
+obsweight=mads_config["mads_obsweight"]
+if isnothing(obsweight)
+    obsweight = ones(Int8, n_o-6)*100
+else
+    println("Make sure that weight length match with targets length")
+end
+
+# check for paramlog
+n_p=length(initial_guess)
+paramlog=mads_config["mads_paramlog"]
+
+if isnothing(paramlog)
+    paramlog = falses(n_p) # for small parameter values (<10-3) this needs to trues
+else
+    println("Make sure that paramlog length match with IC length")
+end
+
+#choose a range for parameter values
+paramdist = []
+mads_paramrange=mads_config["mads_paramrange"]
+if mads_paramrange == "ON"
+    var=mads_config["mads_param_percent_variance"]
+    for i in eachindex(initial_guess)
+        min_r = initial_guess[i] .- initial_guess[i] .* (var / 100)
+        max_r = initial_guess[i] + initial_guess[i] .* (var / 100)
+        push!(paramdist, "Uniform($(min_r), $(max_r))")
+    end
+else
+    paramdist=mads_config["mads_paramdist"]
+end
+
+
+#choose a range for observation values
+obsdist = []
+mads_obsrange=mads_config["mads_obsrange"]
+if mads_obsrange == "ON"   
+    var=mads_config["mads_obs_percent_variance"]
+    for i in eachindex(obs)
+        min_r = max.(obs[i] .- obs[i] .* (var / 100), 0)
+        max_r = obs[i] + obs[i] .* (var / 100)
+        push!(obsdist, "Uniform($(min_r), $(max_r))")
+    end
+end
+
 
 md = Mads.createproblem(initial_guess, targets, TEM_pycall;
     paramkey=mads_config["mads_paramkey"],
-    paramdist=mads_config["mads_paramdist"],
-    obstime=obs_time,
-    #obsweight=[100,100,100,100],
+    paramdist,
+    obstime,
+    obsweight,
+    paramlog,
+    obsdist,
     problemname=mads_config["mads_problemname"])
 
 md["Problem"] = Dict{Any,Any}("ssdr"=>true)
@@ -52,11 +96,11 @@ md["Problem"] = Dict{Any,Any}("ssdr"=>true)
 Mads.showparameters(md)
 Mads.showobservations(md)
 
-#forward_model = Mads.forward(md)
+forward_model = Mads.forward(md)
 
 calib_param, calib_information = Mads.calibrate(md, tolOF=0.01, tolOFcount=4)
 
-Mads.plotmatches(md, calib_param, 
-    xtitle="# of observations", ytitle="GPP",filename="AC2-STEP1-MD1.png")
+Mads.plotmatches(md, calib_param, xtitle="# of observations", 
+		      ytitle="GPP",filename=mads_config["mads_problemname"]*".png")
 
 
