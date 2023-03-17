e27622700138125881fd44424bb6ad3a5b4599cc
Elchin <elchin.jafarov@gmail.com>
2023-03-17 01:39:24 +0000

    cleaned TEM

--- mads_calibration/TEM.py
+++ mads_calibration/TEM.py
@@ -37,7 +37,7 @@ def log_wrapper(message,tag=''):
     Likely will abandon or repurpose this function.
     Not super helpful as a log printer.
     '''
-    print('[SA:{}] {}'.format(tag, message))
+    print('[AC:{}] {}'.format(tag, message))
     try:
         yield
     finally:
@@ -283,110 +283,6 @@ class TEM_model:
         shutil.rmtree(self.work_dir, ignore_errors=True)
         return
     
-    def netcdf2df(self,var_name,TIME,outfolder=''):    
-        '''
-        Input:
-            fdir: path to the resutls folder
-            var_name: output variable name
-            TIME: 'monthly' or 'yearly'
-        Output:
-            df1: dataframe of oname time series for tr run
-        Example:
-        vegc=netcdf2df('/data/workflows/single_run/output/','VEGC','monthly')
-        '''  
-
-        #fname=fdir+var_name+'_'+TIME+'_tr.nc'
-        fname=self.work_dir+'/output/'+var_name+'_'+TIME+'_tr.nc'
-        print(fname)
-
-        if TIME == 'monthly' :
-          period = 15*12
-          freq = 'M'
-        elif TIME == 'yearly':
-          period = 15
-          freq = 'Y' 
-        else:
-          print('Unknown period and freq')
-        #get the 'oname' variable time series from the correspoding output nc files  
-        stg_data = nc.Dataset(fname, 'r')
-
-        if len(stg_data.dimensions)==4:
-            keys=[item for item in stg_data.dimensions.keys()]   
-            df1 = pd.DataFrame(stg_data.variables[var_name][-period:,:,0,0], \
-                     columns=[var_name+'_'+str(idx) for idx in range(stg_data.dimensions[keys[3]].size)])
-        else:
-            df1 = pd.DataFrame(stg_data.variables[var_name][-period:,0,0], columns=[var_name]) 
-        df1 = df1.set_index(pd.Series(pd.period_range("1/2000", freq=freq, periods=period)))
-        df1.index.name = 'date'
-
-        return df1
-
-    def get_TEM_output(self,var_name):
-        '''
-        fdir: '/data/workflows/single_run/output/'
-        var_name: 'VWCLAYER'
-        Example:
-            gpp=get_TEM_output('/data/workflows/single_run/output/','TLAYER')
-        '''
-        #path_to_out=self.work_dir+'/output/'
-        if var_name=='GPP':
-            out=self.netcdf2df(var_name,'monthly')
-            out['Total']= out.sum(axis=1)
-        elif var_name=='ALD':
-            out=self.netcdf2df(var_name,'yearly')
-        else:
-            out=self.netcdf2df(var_name,'monthly')
-
-        return out
-
-    def get_Toolik_obs_out(self, VAROBS, VAROUT):
-
-        '''
-        VAROUT: GPP
-        VAROBS: gpp
-        Output:
-        out: dateframe of outputs
-        obs: dataframe of obervations
-        Example:
-            [obs,out]=get_Toolik_obs_out('gpp','GPP')
-        '''
-        path_to_obser="/data/input-catalog/Toolik/observations/"
-        obs_fname=path_to_obser+"Toolik_"+VAROBS+".csv"
-        obs=pd.read_csv(obs_fname)
-        print(VAROBS+' observ shape (before):',obs.shape)
-        #print(obs.head())
-
-        obs.dropna(inplace=True)
-        obs=obs.set_index('date')
-        formatted_vars=['gpp','10cm_tlayer4','20cm_tlayer5','40cm_tlayer7','snowthick','reco']
-        if VAROBS in formatted_vars:
-            obs.index = pd.to_datetime(obs.index, format = '%Y-%m-%d').strftime('%Y-%m') 
-            #Comment this out for yearly observations (ald, deepdz, shlwdz)
-
-        # NOTE: The index periods should match 
-        obs.index=pd.PeriodIndex(obs.index.values, freq='M')
-        #obs=obs.set_index('date')
-        obs.index.name = 'date'
-        #print('obs:',obs.head())
-
-        #VAROUT
-        #path_to_out='/data/workflows/single_run/output/'
-        #path_to_out=self.work_dir+'/output/'
-        #print(path_to_out,VAROUT)
-        out=self.get_TEM_output(VAROUT)
-        print(VAROUT+' TEM outputs shape (before):',out.shape)
-        print('out:',out.tail())
-
-        common_index = set(obs.index).intersection(out.index)
-        #obs = obs.loc[common_index].sort_values(by='date')
-        #out = out.loc[common_index].sort_values(by='date')
-        obs = obs.loc[list(common_index)].sort_index()
-        out = out.loc[list(common_index)].sort_index()
-        print(VAROBS+' observ shape (after):',obs.shape)
-        print(VAROUT+' TEM outputs shape (after):',out.shape)
-        print()
-
-        return [obs,out]
 
     def run_TEM(self,x):
     
@@ -534,30 +430,3 @@ class TEM_model:
 
         return 
 
-#dvmdostem=TEM_model()
-# The setup is slighly different from the Sensitivity Analysis (SA).
-# We do not include bounds here, there is no need in bounds for the DA. 
-# Similar to SA we have params dictionary in following form
-# {'name': 'cmax', 'val': 210.0, 'cmtnum': 4, 'pftnum': 0}
-# This is dictionary is taking into account multiple pfts
-# set_params setups number of parameters for a given cmtnum.
-# currently (similar to SA) we cannot do that for multiple cmtnums
-# Then we call `model_setup`, which copies all the input 
-# and parameter files from dvmdostem to the `work_dir`. This allows to not interfere
-# with setup in the dvmdostem folder (i.e. run DA in the designated folder). 
-# NOTE: this formulation is different from SA, because we run everything in designated 
-# work_dir folder. The pymc will do the required optimization and parallelization on its own.
-# Our goal is to create a `custom_model` function which could update parameters in the model. 
-
-# To make this parallel, use setup from SA
-# generate unique xxxx number based on number of processors that will be used
-# for example sample will use 4 cores
-# then `run` should have 16 unique samples 
-# random.sample(range(1000, 9999), 4)
-#dvmdostem.set_params(cmtnum=4, params=['cmax','cmax'], pftnums=[0,1])
-#print(dvmdostem.params)
-#print()
-#dvmdostem.clean()
-#dvmdostem.setup()
-#dvmdostem.run()
-#Need to wirte the run function 
