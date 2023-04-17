b2ec045bf2ddf7eca2319ba38cdea3efdca75c90
Aiza Kabeer <akabeer@woodwellclimate.org>
2023-04-17 18:04:22 +0000

    update to use yaml config file

--- mads_calibration/equilibrium_check.py
+++ mads_calibration/equilibrium_check.py
@@ -1,3 +1,14 @@
+# Equilibrium check adapted  from Helene's code for yaml SA/calib set up
+# Must manually adjust:
+# 	line 28: config_file_name
+#	lines 33-42: choose appropraite paths, file names and variables
+#		NOTE:this code only works for ONE target at a time
+#			 SPECIFY index of target you're interested with the variable j
+#			 (indices match the order of targets as loaded from yaml file)
+# Example: python3 equilibrium_check.py
+# Aiza
+# 04/17/2023
+
 import xarray as xr
 import glob
 from scipy import stats
@@ -5,17 +16,19 @@ import pandas as pd
 import os,sys
 sys.path.append(os.path.join('/work','calibration'))
 import calibration_targets
+import yaml
+sys.path.append(os.path.join('/work','mads_calibration'))
+import TEM
 
-# sys.path.append(os.path.join('/work','mads_calibration'))
-# import TEM
+def get_cofig_file(config_file_name):
+    dvmdostem=TEM.TEM_model(config_file_name)
+    dvmdostem.set_params(dvmdostem.cmtnum, dvmdostem.paramnames, dvmdostem.pftnums)
+    return dvmdostem
 
-# def get_cofig_file(config_file_name):
-#     dvmdostem=TEM.TEM_model(config_file_name)
-#     dvmdostem.set_params(dvmdostem.cmtnum, dvmdostem.paramnames, dvmdostem.pftnums)
-#     return dvmdostem
-
-# tem = get_cofig_file('config-step2-md3_noprior.yaml')
-# targets=tem.get_targets(1)
+tem = get_cofig_file('config-step2-md3_noprior.yaml')
+CMTNUM = tem.cmtnum
+var_tar_all=tem.target_names #this loads specified targets from config file
+var_pairs=tem.caltarget_to_ncname_map # this loads all target output pairs
 
 POD='/data/workflows/STEP2-MD3-CR_6' # path to SA sample folders
 PD='/data/workflows'
@@ -23,12 +36,15 @@ path_SA='SA/'
 params='sample_matrix_STEP2-MD3-CR-noprior.csv'
 model='results_STEP2-MD3-CR-noprior.txt'
 N = 1000 #sample size
-CMTNUM = 3
-VARtar = 'GPPAllIgnoringNitrogen'
-VARout = 'INGPP'
+j = 0 #index of the relevant target (in var_tar_all)
 neqy = 30 #num years to test equilibrium
 ncaly = 10 #num years to test calibration
 eco_file = '/nmax_eq_results.csv' #name of csv with calculated slope, cv and p value
+
+### Load VARtar and matching VARout
+out_matches = [match for match in var_pairs if var_tar_all[j] in match]
+VARtar = var_tar_all[j]
+VARout = out_matches[0][1]
 
 ### Read parameter values
 sm = pd.read_csv(os.path.join(POD,'sample_matrix.csv'))
@@ -37,7 +53,8 @@ sm.reset_index(inplace=True)
 sm = sm.rename(columns={"index": "sample"})
 sm.describe()
 
-### Read the target  
+### Read the target 
+# targets=tem.get_targets(1) #doesn't work with yaml config? 
 for cmtname, data in calibration_targets.calibration_targets.items():
 	if str(CMTNUM) == str(data.get("cmtnumber")):
 		caltargets = data
@@ -49,6 +66,7 @@ targets = targets.rename(columns={"index": "pft"})
 targets = targets.rename(columns={0: VARtar})
 
 ### Read out the outputs and test for equilibrium and stabililty and compute RMSE
+print('Calculating cv, p, slope and rmse to check eq for target '+VARtar+ ' and output '+VARout+'\n')
 results_eco = pd.DataFrame() 
 results_pft = pd.DataFrame()
 failed_run = []
@@ -102,15 +120,15 @@ for i in range(int(N)):
 	results_eco = pd.concat([results_eco,df],ignore_index=True)
 	results_pft = pd.concat([results_pft,out],ignore_index=True)
 
-results_eco.to_csv(os.path.join(path_SA + eco_file), sep=',',index=False)
+results_eco.to_csv(os.path.join(POD + eco_file), sep=',',index=False)
 
 ### To remove results that didn't meet eq check
 df_param = pd.read_csv(path_SA+params)
 df_model = pd.read_csv(path_SA+model,header=None)
 good_df_param = df_param.drop(failed_run)
 good_df_model = df_model.drop(failed_run)
-good_df_param.to_csv(os.path.join(path_SA + 'good_df_param'), sep=',',index=False)
-good_df_model.to_csv(os.path.join(path_SA + 'good_df_model'), sep=',',index=False)
+good_df_param.to_csv(os.path.join(path_SA + 'good_df_param.csv'), sep=',',index=False)
+good_df_model.to_csv(os.path.join(path_SA + 'good_df_model.csv'), sep=',',index=False)
 
 
 # select the 5 best performing samples
