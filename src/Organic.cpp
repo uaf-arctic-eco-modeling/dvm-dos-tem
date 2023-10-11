383a0913f880dff5d28644cf54b735b2f5efcf71
bmaglio <bmaglio@alaska.edu>
2023-10-11 07:32:53 -0800

    Reworking layer forcing scheme using a scaling between uniform and linearly increasing thickness

--- src/Organic.cpp
+++ src/Organic.cpp
@@ -6,82 +6,104 @@ Organic::Organic() {
 Organic::~Organic() {
 };
 
-void Organic::ShlwThickScheme(const double & totthickness) {
+void Organic::ShlwThickScheme(const double & shlw_totthickness, const double & deep_totthickness) {
   //BM: implementing a layer "forcing" scheme to test organic layer resolution on soil thermal and hydrological regimes
+  //    This is based on assuming uniform or linearly increasing layers. A scaling factor is used to find an optimum between uniform and linear schemes.
 
-  // Initialize total thickness of shlw horizon
-  shlwthick = totthickness;
+  // Initialize total thickness of shlw horizon - do I even need to initialize these?
+  shlwthick = shlw_totthickness;
+  deepthick = deep_totthickness;
 
   // Initialize shlw thicknesses as fill values
   std::fill(begin(shlwdz), end(shlwdz), MISSING_D);
 
-  // Initialize shlwnum=0 prior to assignment
+  // Initialize shlwnum=0 and deepnum=0 prior to assignment - necessary?
   shlwnum = 0;
+  deepnum = 0;
 
-  // Force number of shlw layers to MAX_SLW_LAY from layerconst.h
+  // Force number of layers to MAX_LAY values from layerconst.h
   shlwnum = sizeof(shlwdz) / sizeof(double);
+  deepnum = sizeof(deepdz) / sizeof(double);
 
-  // Creating ratios based on the number of shlw layers for layer division
-  // layDenominator used to determine minimum fraction of layers based on shlwnum
-  // layNumerator used to find ratio, excluding final layer, to avoid rounding errors 
-  // (e.g. shlwnum=2, layNumerator=1, layDenominator=3, shlwdz[0]=1/3 * shlwthick, shlwdz[1]=shlwthick - 1/3 * shlwthick=2/3 * shlwthick)
-  int layNumerator = (shlwnum - 1) * (shlwnum) / 2;
-  int layDenominator = shlwnum * (shlwnum + 1) / 2;
+  //Creating uniform shlw thickness, uniform_deep used for scaling factor
+  double uniform_shlw = shlwthick / shlwnum;
+  double uniform_deep = deepthick / deepnum;
+  //Creating linear thickness relation (y = mx + c, y=shlwdz, x=layer index, m=increase (gradient), c=fstshlwdz = (1 * m))
+  double gradient_shlw = shlwthick / (0.5*shlwnum*(shlwnum+1));
+  double intercept_shlw = gradient_shlw;//redefined for assignment clarity
+  //Creating linear relationships between lstshlwdz and fstdeep for scaling factor
+  double linear_shlw = gradient_shlw * shlwnum;
+  double linear_deep = deepthick / (0.5*deepnum*(deepnum+1));
+
+  //Scaling factor between uniform and linear thickness based on full organic layer properties (shlwnum, deepnum, shlwthick, deepthick)
+  //This is calculated by assuming y=lstshlwdz=fstdeepdz, and shlw uniform = Us, linear = Ls, and deep uniform = Ud, linear = Ld. scaling_factor = F
+  //for lstshlw and fstdeep, y = F * Us + (1-F)*Ls = F * Ud + (1-F)*Ld - then solving for F = Ld - Ls / (Us - Ls + Ld - Ud)
+  double scaling_factor = (linear_deep - linear_shlw) / (uniform_shlw - linear_shlw + linear_deep - uniform_deep);
+  
+  //Defining double for storing cumulative thickness to calculate final layer thickness without encountering rounding errors:
+  double final_layer = 0.0;
 
   // Looping through layer assigning thickness (excluding final layer to avoid rounding errors)
   for (int i = 0; i < (shlwnum-1); i++) {
-    shlwdz[i]= (i+1) * (shlwthick / layDenominator);
+    shlwdz[i] = (scaling_factor * uniform_shlw) + (1 - scaling_factor) * (i * gradient_shlw + intercept_shlw);
+    final_layer += shlwdz[i];
   }
 
   // Assigning final layer by subtracting total preassigned layer thicknesses
-  shlwdz[shlwnum-1] = shlwthick - layNumerator*shlwthick/layDenominator;
+  shlwdz[shlwnum-1] = shlwthick - final_layer;
 
-  // Assigning lstshlwdz to be used in DeepThickScheme
+  // Assigning lstshlwdz to be used in DeepThickScheme - not currently used for forcing but leaving for final scheme
   if (shlwnum>0) {
     lstshlwdz = shlwdz[shlwnum-1];
   }
+
 };
 
-void Organic::DeepThickScheme(const double & totthickness) {
-  //BM: implementing a layer "forcing" scheme to test organic layer resolution on soil thermal and hydrological regimes
+void Organic::DeepThickScheme(const double & shlw_totthickness, const double & deep_totthickness) {
+   //BM: implementing a layer "forcing" scheme to test organic layer resolution on soil thermal and hydrological regimes
+  //    This is based on assuming uniform or linearly increasing layers. A scaling factor is used to find an optimum between uniform and linear schemes.
 
-  // Initialize total thickness of deep horizon 
-  deepthick = totthickness;
+  // Initialize total thickness of shlw horizon - do I even need to initialize these?
+  shlwthick = shlw_totthickness;
+  deepthick = deep_totthickness;
 
-  // Initialize deep thicknesses as fill values
+  // Initialize shlw thicknesses as fill values
   std::fill(begin(deepdz), end(deepdz), MISSING_D);
 
-  // Initialize deepnum=0 prior to assignment
+  // Initialize shlwnum=0 and deepnum=0 prior to assignment - necessary?
+  shlwnum = 0;
   deepnum = 0;
 
-  // Force number of deep layers to MAX_DEP_LAY from layerconst.h
-  deepnum = sizeof(deepdz) / sizeof(double);              
+  // Force number of layers to MAX_LAY values from layerconst.h
+  shlwnum = sizeof(shlwdz) / sizeof(double);
+  deepnum = sizeof(deepdz) / sizeof(double);
 
-  // Creating ratios for layer thickness assignment based on a linear extrapolation from lstshlwdz
-  // Equivalent to y=mx+c, where y is deepdz and x is layer number in deep horizon
-  // Used to create a linearly increasing deepdz while conserving deepthick 
-  //layDenominator used to determine minimum fraction of layers based on deepnum
-  double layDenominator = deepnum * (deepnum + 1) / 2; //layer ratio denominator
+  //Creating uniform shlw thickness, uniform_deep used for scaling factor
+  double uniform_shlw = shlwthick / shlwnum;
+  double uniform_deep = deepthick / deepnum;
+  //Creating linear thickness relation (y = mx + c, y=shlwdz, x=layer index, m=increase (gradient), c=fstshlwdz = (1 * m))
+  double gradient_deep = deepthick / (0.5*deepnum*(deepnum+1));
+  double intercept_deep = gradient_deep;//redefined for assignment clarity
+  //Creating linear relationships between lstshlwdz and fstdeep for scaling factor
+  double linear_shlw = shlwthick / (0.5*(shlwnum+1));
+  double linear_deep = gradient_deep;
 
-  // layScalar is used to set the ratio between lstshlwdz and deepthick - this may need to be multiplied if fstdeepdz>lstshlwdz (i.e. layScalar * 1.05, 5% increase)
-  double layScalar = (lstshlwdz / deepthick);
-  // layGradient is used to define the rate of thickness increase between deep layers based on deepnum, layDenominator, and layScalar (equivalent to m in y=mx+c)  
-  double layGradient = ((deepnum/layDenominator) - (2 * layScalar) + (1/layDenominator)) / (deepnum - 1);
-  // layIntercept is used to set zeroth layer thickness ratio of deepthick (equivalent to c in y=mx+c)
-  double layIntercept = layScalar - layGradient;
-  // layRemainder initialized to zero and assigned to as deepdz is assigned for subtraction at final layer and avoiding rounding errors
-  double layRemainder = 0.0;
+  //Scaling factor between uniform and linear thickness based on full organic layer properties (shlwnum, deepnum, shlwthick, deepthick)
+  //This is calculated by assuming y=lstshlwdz=fstdeepdz, and shlw uniform = Us, linear = Ls, and deep uniform = Ud, linear = Ld. scaling_factor = F
+  //for lstshlw and fstdeep, y = F * Us + (1-F)*Ls = F * Ud + (1-F)*Ld - then solving for F = Ld - Ls / (Us - Ls + Ld - Ud)
+  double scaling_factor = (linear_deep - linear_shlw) / (uniform_shlw - linear_shlw + linear_deep - uniform_deep);
+  
+  //Defining double for storing cumulative thickness to calculate final layer thickness without encountering rounding errors:
+  double final_layer = 0.0;
 
   // Looping through layer assigning thickness (excluding final layer to avoid rounding errors)
   for (int i = 0; i < (deepnum-1); i++) {
-    deepdz[i]= ((i+1) * layGradient + layIntercept) * deepthick;
+    deepdz[i] = (scaling_factor * uniform_deep) + (1 - scaling_factor) * (i * gradient_deep + intercept_deep);
+    final_layer += deepdz[i];
+  }
 
-    layRemainder += deepdz[i];
-
-    }
-
-  // Assigning final layer by subtracting layRemainder (assigned thicknesses) to avoid rounding errors
-  deepdz[deepnum-1] = deepthick - layRemainder;
+  // Assigning final layer by subtracting total preassigned layer thicknesses
+  deepdz[deepnum-1] = deepthick - final_layer;
 };
 
 // if shlw peat thickness from input soil profile
