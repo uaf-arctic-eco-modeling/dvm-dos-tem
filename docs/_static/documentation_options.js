69d29e3c4d6f35089bc8676d3f777fc62ae1ebe6
Tobey Carman <tobeycarman@gmail.com>
2023-11-02 13:23:19 -0800

    Publising site from docs_update (v0.7.0-58-g27e9e2e9)

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v0.6.2',
+    VERSION: 'v0.7.0-58-g27e9e2e9',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
