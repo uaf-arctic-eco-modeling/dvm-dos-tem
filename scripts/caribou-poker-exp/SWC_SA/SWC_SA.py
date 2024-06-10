87e1927abfa7f3e4e0544870228d01e3e6671966
amullen01 <andrew.mullen39@gmail.com>
2024-06-10 15:49:00 +0000

    usprr notebooks

---
+++ scripts/caribou-poker-exp/SWC_SA/SWC_SA.py
@@ -0,0 +1,209 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import sys
+import os
+#os.environ['PYTHONPATH']='/work/scripts'
+# setting path
+sys.path.append('/work/scripts/')
+sys.path.append('/work/scripts/util')
+sys.path.append('/work/scripts/drivers')
+import pandas as pd
+import seaborn as sns
+import Sensitivity_AM as sa
+import output as ou
+
+
+# # parameters influencing soil hydro/thermal state
+# ### cmt_envground.txt
+# --------------------------------------------------------
+# #### tcsolid(m): Soil thermal conductivity for moss (W/mK)
+# current value: 0.25, range: 0.005-0.5
+# 
+# *Ekici et al. 2015,Jiang et al. 2015, O’Donnell et al. 2009*
+# 
+# #### tcsolid(f): Soil thermal conductivity for fibric (W/mK)
+# current value: 0.25, range: 0.005-0.5
+# 
+# *Ekici et al. 2015,Jiang et al. 2015, O’Donnell et al. 2009*
+# 
+# #### tcsolid(h): Soil thermal conductivity for humic (W/mK)
+# current value: 0.25, range: 0.02-2.0
+# 
+# *Ekici et al. 2015,Jiang et al. 2015, O’Donnell et al. 2009*
+# 
+# ---------------------------------------------------------
+# #### porosity(m): porosity for moss layers (m3/m3)
+# current value: 0.98, range: 0.85-0.99
+# 
+# *O’Donnell et al. 2009*
+# 
+# #### porosity(f): porosity for fibric layers  (m3/m3)
+# current value: 0.95, range: 0.85-0.99
+# 
+# *O’Donnell et al. 2009*
+# 
+# #### porosity(h): porosity for humic layers  (m3/m3)
+# current value: 0.8, range: 0.7-0.9
+# 
+# *O’Donnell et al. 2009*
+# 
+# --------------------------------------------------------
+# #### bulkden(m): bulk density for moss (g/m3)
+# current value: 25,000, range: 10,000 - 80,000
+# 
+# *Tuomi et al. 2020, Rodionov et al. 2007, O’Donnell et al. 2009*
+# 
+# #### bulkden(f): bulk density for fibric (g/m3)
+# current value: 51,000, range: 20,000 - 200,000
+# 
+# *Tuomi et al. 2020, Rodionov et al. 2007, O’Donnell et al. 2009*
+# 
+# #### bulkden(h): bulk density for humic (g/m3)
+# current value: 176,000, range: 100,000 - 800,000
+# 
+# *Tuomi et al. 2020, Rodionov et al. 2007, O’Donnell et al. 2009*
+# 
+# --------------------------------------------------------
+# #### hksat(m): hydraulic conductivity at saturation for moss (mm/s)
+# current value: 0.15, range: 0.0002 - 30
+# 
+# *Ekici et al. 2015, Letts et al. 2000, Liu et al. 2019*
+# 
+# #### hksat(f): hydraulic conductivity at saturation for fibric (mm/s)
+# current value: 0.28, range: 0.0002 - 30
+# 
+# *Ekici et al. 2015, Letts et al. 2000, Liu et al. 2019*
+# 
+# #### hksat(h): hydraulic conductivity at saturation for humic (mm/s)
+# current value: 0.002, range: 0.00004 - 2.01
+# 
+# *Ekici et al. 2015, Letts et al. 2000, Liu et al. 2019*
+# 
+# ---------------------------------------------------------
+# #### nfactor(s): Summer nfactor
+# current value: 1.5, range: 0.2-2.0
+# 
+# *Kade et al. 2006, Klene et al. 2001*
+# 
+# #### nfactor(w): Winter nfactor
+# current value: 1.0, range: 0.4-1.0
+# 
+# *Kade et al. 2006, Klene et al. 2001*
+# 
+# ---------------------------------------------------------
+# #### snwalbmax
+# current value: 0.8, range: 0.7-0.85
+# 
+# *Te Beest et al. 2016, Petzold et al. 1975, Loranty et al. 2011*
+# 
+# #### snwalbmin
+# current value: 0.4, range: 0.4-0.6
+# 
+# *Te Beest et al. 2016, Petzold et al. 1975, Loranty et al. 2011*
+# 
+# ---------------------------------------------------------
+# ### cmt_dimground.txt
+# ---------------------------------------------------------
+# #### snwdenmax (kg/m3)
+# current value: 250, range: 100-800
+# 
+# *Domine et al. 2016, Gerland et al. 1999, Muskett 2012*
+# 
+# #### snwdennew (kg/m3)
+# current value: 50, range: 10-250
+# 
+# *Domine et al. 2016, Gerland et al. 1999, Muskett 2012*
+# 
+# 
+# ### cmt_bgcsoil.txt
+# ---------------------------------------------------------
+# #### rhq10
+# current value: 2, range: 1.6-2.4
+# #### rhq10_w
+# current value: 2, range: 0.85-0.99
+
+work_dir='/data/workflows/US-Prr_SWC_SA'
+opt_run_setup='--tr-yrs=121 --sp-yrs=300 --eq-yrs=500 '
+
+driver = sa.SensitivityDriver(work_dir = work_dir, clean=True)
+driver.site = '/data/input-catalog/caribou-poker_merged/'
+driver.opt_run_setup = opt_run_setup
+
+#US-Prr
+driver.PXx ='1'
+driver.PXy='0'
+#
+#BONA
+#driver.PXx ='0'
+#driver.PXy='3'
+#
+
+params = [#'bulkden(m)', 'bulkden(m)', 'bulkden(m)',
+          'hksat(m)','hksat(f)','hksat(h)',
+          'tcsolid(m)', 'tcsolid(f)', 'tcsolid(h)',
+          'porosity(m)', 'porosity(f)', 'porosity(h)',
+          'nfactor(s)', 'nfactor(w)',
+          'rhq10']#, 'rhq10_w']
+#hksat-m .12-.15
+#.25-.35
+#.01-0.33
+#US-Prr
+#bounds = [[0.0005056, 0.003922], [0.0000539, 0.004125], [0.000040, 0.000620], #[0.000040, ]
+#          [0.005, 0.5], [0.005, 0.5], [0.02, 2],
+#          [0.83, 0.98], [0.8, 0.83], [0.7, 0.8],
+#          [0.2, 2.5], [0.1, 1],
+#          [1.6, 2.4]]#, [1.6, 2.4]]
+
+bounds = [#[22000,28000],[58000, 63000],[190000, 210000],
+          [0.05, 0.2], [0.05, 0.2], [0.00040, 0.0020], #[0.000040, ]
+          [0.15, 0.35], [0.1, 0.3], [0.1, 0.3],
+          [0.94, 0.98], [0.78, 0.82], [0.7, 0.8],
+          [0.5, 1.0], [0.3, 1.0],
+          [1.6, 2.4]]
+
+
+#BONA-Black-Spruce
+#bounds = [[0.0005056, 0.003922], [0.0000539, 0.004125], [0.000040, 0.000620], #[0.000040, ]
+#          [0.005, 0.5], [0.005, 0.5], [0.02, 2],
+#          [0.83, 0.98], [0.8, 0.83], [0.7, 0.8],
+#          [0.2, 2.5], [0.1, 1],
+#          [1.6, 2.4]]#, [1.6, 2.4]]
+
+
+driver.logparams = [#0, 0, 0,
+                    1, 1, 1,
+                    1, 1, 1,
+                    0, 0, 0,
+                    0, 0,
+                    0]#, 0]
+
+driver.outputs = [
+      { 'name': 'GPP', 'type': 'flux'},
+      { 'name': 'RH','type': 'flux'},
+      { 'name': 'ALD','type': 'flux'},
+      { 'name': 'LWCLAYER','type': 'layer'},
+      { 'name': 'VWCLAYER','type': 'layer'},
+      { 'name': 'TLAYER','type': 'layer'},
+      { 'name': 'LAYERDEPTH','type': 'layer'},
+      { 'name': 'LAYERDZ','type': 'layer'},
+      { 'name': 'LAYERTYPE','type': 'layer'},
+    ]
+
+driver.design_experiment(Nsamples = 50, cmtnum = 13, params = params,
+                         bounds=bounds, pftnums = [None]*len(params), sampling_method='uniform')
+
+
+driver.clean()
+driver.setup_multi()
+driver.run_all_samples()
+
+
+driver.sample_matrix
+
+
+
+
+
+
+
---
+++ scripts/caribou-poker-exp/soil_layer_synthesis/Output_synthesis.sh
@@ -0,0 +1,125 @@
+
+#!//usr/local/bin/bash
+
+# Author: Helene Genet, UAF
+# Creation date: Jan. 25 2022
+# Purpose: general script to synthesis TEM outputs for data analysis
+
+
+
+### INFORMATION REQUIRED
+
+# path to the directory of TEM input files used to produce the TEM outputs
+inputdir="/data/input-catalog/caribou-poker/"
+# path to the TEM raw output directory
+rawoutdir="/data/workflows/poker_flats_merged_data/output/"
+#tar -zxvf "${rawoutdir}.tar.gz"
+# path to the directory containing the python scripts associated to this bash (e.g. Layer_var_synth.py)
+scriptdir="/work/scripts/caribou-poker-exp/soil_layer_synthesis/"
+# path to the directory to store output synthesis files
+outdir="/work/scripts/caribou-poker-exp/soil_layer_synthesis/"
+# historical period starting and ending years
+hist_start=1901
+hist_end=2015
+# projection period starting and ending years
+proj_start=2016
+proj_end=2100
+# list of simulation scenarios of interest: pr, eq, sp, tr, sc for pre-run, equilibrium, spin-up, historical and scenario runs
+#sclist=(tr sc)
+sclist=(tr)
+# name of the scenario run
+scname=usprr
+# name of the variable of interest
+var=LWCLAYER
+# for variables with soil layer dimension, specify the soil depths, in meter, at which you woould like the variable to be synthesized
+depthlist=(0.05,0.10,0.20)
+
+
+
+### GETTING STARTED...
+
+cd $rawoutdir
+mkdir $outdir
+
+
+
+### DETERMINE OUTPUTFILE TIME RESOLUTION AND LIST OF DIMENSIONS
+
+## 1- Time resolution
+
+# list the time frequency of all output files of interest
+freq=()
+for sc in "${sclist[@]}"; do
+  #1- determine frequency of outputs
+  if [[ $(basename "${rawoutdir}${var}_"*"_${sc}.nc") == *"monthly"* ]];then
+    fq="monthly"
+  elif [[ $(basename "${rawoutdir}${var}_"*"_${sc}.nc") == *"yearly"* ]];then
+    fq="yearly";
+  fi
+  freq=("${freq[@]}" "${fq[@]}")
+done
+# check that the time resolution is the same for all output files.
+echo ${freq[@]}
+mapfile -t tres < <(printf "%s\n" "${freq[@]}" | sort -u)
+if (( ${#tres[@]} > 1 ));then
+  echo "All output files do not have the same time frequency. Please homogenize the time resolution of the output files. [To do so, you can rather reproduce the simulations with homogenous time resolution, or use the ncwa operator (nco) to rather sum (fluxes) or take the december value (stocks) of monthly outputs to generate yearly outputs which are the coarsest resolution of TEM outputs]."
+else  
+  echo "The time resolution of the output files is: ${tres[@]}"
+fi
+
+
+## 2- Dimensions
+
+# list the dimensions from all output files of interest and check they are all the same.
+prevdimlist=()
+for sc in "${sclist[@]}"; do
+  echo $sc;
+  ncdump -h "${rawoutdir}${var}_"*"_${sc}.nc" > a.txt
+  IFS=' ' read -r -a dimlist <<< "`grep $var'(' a.txt | awk -F"[()]" '{print $2}' | sed 's/ //g' | sed 's/,/ /g'`"
+  if (( ${#prevdimlist[@]} > 0 ));then
+    diff=$(diff <(printf "%s\n" "${dimlist[@]}") <(printf "%s\n" "${prevdimlist[@]}"))
+    if [[ ! -z "$diff" ]]; then
+      echo "The list of dimensions is not the same in all output files of interest. Please homogenize. [To do so, you can rather reproduce the simulations with homogenous dimensions, or use the ncwa operator (nco) to sum or averag the variable values along the dimensions that are missing in some of the outputs files of interest]."
+      dimlist=()
+      break
+    fi
+  fi
+  prevdimlist=("${dimlist[@]}")
+done
+echo "The list of dimensions in the output files is: ${dimlist[@]}"
+
+
+
+
+### PRODUCE SOIL LAYER OUTPUTS OF COMPARABLE DEPTH
+
+
+for sc in "${sclist[@]}"; do
+  echo "scenario ${sc}"
+  # make sure that layer dz and layer types are present
+  if [[ ! -f "${rawoutdir}LAYERTYPE_monthly_${sc}.nc" ]];then
+    echo "LAYERTYPE for ${sc} mode doesn't exist. Yet it is need to run this procedure. Break";
+    break
+  elif [[ ! -f "${rawoutdir}LAYERDZ_monthly_${sc}.nc" ]];then
+    echo "LAYERDZ for ${sc} mode doesn't exist. Yet it is need to run this procedure. Break";
+    break
+  fi
+  cp "${rawoutdir}LAYERTYPE_monthly_${sc}.nc" "${rawoutdir}SOILSTRUCTURE_monthly_${sc}.nc" 
+  ncks -A -h "${rawoutdir}LAYERDZ_monthly_${sc}.nc" "${rawoutdir}SOILSTRUCTURE_monthly_${sc}.nc" 
+  # export the necessary info to run the python code
+  export scrun=$sc
+  export inpath=$rawoutdir
+  export outpath=$outdir
+  export ncvar=$var
+  export timeres=${tres[0]}
+  export dmnl=${dimlist[@]}
+  for i in "${dimlist[@]:1}"; do
+    dmnl+=,$i
+  done
+  export dl=${depthlist[0]}
+  for i in "${depthlist[@]:1}"; do
+    dl+=,$i
+  done
+  #run the python script for linear interpolation
+  python3 "${scriptdir}Layer_var_synth.py" 
+done
