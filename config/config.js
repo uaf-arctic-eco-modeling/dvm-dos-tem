e8ed90e75bc70b6176a7d622797d97f08bea7777
bmaglio <bmaglio@alaska.edu>
2026-02-20 08:28:39 -0900

    Updates for regional tile test
    - ch4: true in config file
    - outspec ch4 and CMTNUM additions
    - parameter files, inserted bonanza creek fen calibration instead of Igarka bog to test response

--- config/config.js
+++ config/config.js
@@ -129,7 +129,7 @@
     "dynamic_lai": 1,                   // from model (1) or from input (0)
     "baseline_start": 1901,  //start year for baseline EQ climate
     "baseline_end": 1931,     //end year for baseline EQ climate
-    "ch4": false, //controlled at the stage level by bgc settings above
+    "ch4": true, //controlled at the stage level by bgc settings above
     "run-on": false, //enable run-on (use calculated runoff as input) only if poorly draining pixel
     "groundwater": false //enable calibrated fixed groundwater recharge only if input if poorly draining pixel
 //    //"dynamic_climate": 0,
